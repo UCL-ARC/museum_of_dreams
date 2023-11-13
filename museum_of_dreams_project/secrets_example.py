2516e900119247188dfb7a38bd2394c908b55591
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-13 14:06:54 +0000

    setting up basic secrets

---
+++ museum_of_dreams_project/secrets_example.py
@@ -0,0 +1,2 @@
+secret_key = ""
+db_pw = ""
--- museum_of_dreams_project/settings.py
+++ museum_of_dreams_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from . import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4(a2p@akg1+ns^y&sl2*iw-1d%l2j0wsc@&*+xo#i-d=wanp5_"
+SECRET_KEY = secrets.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +82,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "mod-mysql-db",
         "USER": "modreams",
-        "PASSWORD": "MuseumofDreams",
+        "PASSWORD": secrets.db_pw,
         "HOST": "mod-mysql-db.cxbuhtmmygnm.eu-west-2.rds.amazonaws.com",
         "PORT": "3306",
     },
