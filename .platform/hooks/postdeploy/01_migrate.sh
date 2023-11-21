6114d62053fa8deb776e9e88781fda9e8751940d
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-21 12:29:30 +0000

    using shell script instead

---
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -0,0 +1,25 @@
+# container_commands:
+#   01_migrate:
+#     command: "source /var/app/venv/*/bin/activate && python manage.py migrate"
+#     leader_only: true
+# option_settings:
+#   aws:elasticbeanstalk:application:environment:
+#     DJANGO_SETTINGS_MODULE: museum_of_dreams_project.settings
+
+
+#!/bin/bash
+
+source "$PYTHONPATH/activate" && {
+    
+    if [[ $EB_IS_COMMAND_LEADER == "true" ]];
+    then 
+        # log which migrations have already been applied
+        python manage.py showmigrations;
+        
+        # migrate
+        python manage.py migrate --noinput;
+    else 
+        echo "this instance is NOT the leader";
+    fi
+    
+}
