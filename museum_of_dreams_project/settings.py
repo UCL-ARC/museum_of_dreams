684c598f92f389e52ace2f9ab9ca6b2ab3e93ea6
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-23 17:04:28 +0000

    path?

--- museum_of_dreams_project/settings.py
+++ museum_of_dreams_project/settings.py
@@ -3,6 +3,6 @@ import os
 IS_LOCAL_DEV = os.environ.get("LOCAL_DEV", "False")
 
 if IS_LOCAL_DEV:
-    from settings.local import *
+    from .settings.local import *
 else:
-    from settings.aws import *
+    from .settings.aws import *
