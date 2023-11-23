9db2d436d8816543e8e21cd1786c6473cc84e0fe
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-23 14:39:44 +0000

    testing actor film link

--- mod_app/tests/test_models.py
+++ mod_app/tests/test_models.py
@@ -20,9 +20,20 @@ class TestFilm(TestCase):
 class TestActor(TestCase):
     def setUp(self):
         Actor.objects.create(name="test actor")
+        Film.objects.create(
+            title="test film",
+            release_date=2023,
+        )
 
     def test_actor_creation(self):
         actor = Actor.objects.get(name="test actor")
 
         self.assertTrue(actor)
         self.assertEqual(actor.name, "test actor")
+
+    def test_actor_film_link(self):
+        actor = Actor.objects.get(name="test actor")
+        film = Film.objects.get(title="test film")
+
+        actor.films.add(film)
+        self.assertQuerySetEqual(film.actors.all(), [actor])
