668470cb14be2aff7a08d63064bd49e97bacf506
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-23 17:44:24 +0000

    trying postdeploy script

---
+++ .platform/db-migrate.sh
@@ -0,0 +1,15 @@
+if [ -f /var/app/current/manage.py ]; then
+    # Start venv
+    cd /var/app
+    source venv/staging-LQM1lest/bin/activate
+    cd current 
+
+    # Run migrations
+    python manage.py migrate
+
+    # Collect static files
+    python manage.py collectstatic
+else
+    echo "manage.py not found."
+    exit 1
+fi
