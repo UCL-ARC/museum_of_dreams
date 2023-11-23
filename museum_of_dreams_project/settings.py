68624658358f36d7689c8e938142bebc93d1083e
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-23 18:18:40 +0000

    testing settings vars

--- museum_of_dreams_project/settings.py
+++ museum_of_dreams_project/settings.py
@@ -6,3 +6,10 @@ if IS_LOCAL_DEV == True:
     from .settings_files.local import *
 else:
     from .settings_files.aws import *
+
+    print(
+        "DJANGO_SETTINGS_MODULE:",
+        os.environ.get("DJANGO_SETTINGS_MODULE"),
+        "STATIC_URL:",
+        os.environ.get("STATIC_URL"),
+    )
