6a20232747e05926f8b75cb44ed1435341e4d5bc
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-24 13:33:23 +0000

    tweaks

--- museum_of_dreams_project/settings.py
+++ museum_of_dreams_project/settings.py
@@ -1,13 +1,12 @@
 import os
 
-IS_LOCAL_DEV = os.environ.get("LOCAL_DEV", "False")
+IS_LOCAL_DEV = os.environ.get("LOCAL_DEV", False)
 print("IS_LOCAL_DEV", IS_LOCAL_DEV)
 
-if IS_LOCAL_DEV != False:
+if IS_LOCAL_DEV is not False:
+    print("LOCAL SETTINGS USED")
     from .settings_files.local import *
 
-    print("LOCAL SETTINGS USED")
 else:
-    from .settings_files.aws import *
-
     print("AWS SETTINGS USED")
+    from .settings_files.aws import *
