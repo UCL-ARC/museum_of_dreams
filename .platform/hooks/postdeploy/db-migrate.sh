a7b923d35eed856fe3d3bebbde1ab2275c2d0290
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-24 12:34:15 +0000

    seeing if it works without the shell script

--- .platform/hooks/postdeploy/db-migrate.sh
+++ .platform/hooks/postdeploy/db-migrate.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-if [ -f /var/app/current/manage.py ]; then
-    # Start venv
-    cd /var/app
-    source venv/staging-LQM1lest/bin/activate
-    cd current 
+# #!/bin/bash
+# if [ -f /var/app/current/manage.py ]; then
+#     # Start venv
+#     cd /var/app
+#     source venv/staging-LQM1lest/bin/activate
+#     cd current 
 
-    # Run migrations
-    python manage.py migrate
+#     # Run migrations
+#     python manage.py migrate
 
-    # Collect static files
-    python manage.py collectstatic --no-input
-else
-    echo "manage.py not found."
-    exit 1
-fi
+#     # Collect static files
+#     python manage.py collectstatic --no-input
+# else
+#     echo "manage.py not found."
+#     exit 1
+# fi
