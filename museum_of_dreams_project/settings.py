ada8fa85bc0d77c122972aa776de348a77711643
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-24 13:30:29 +0000

    some print statements to make it clear

--- museum_of_dreams_project/settings.py
+++ museum_of_dreams_project/settings.py
@@ -5,5 +5,9 @@ print("IS_LOCAL_DEV", IS_LOCAL_DEV)
 
 if IS_LOCAL_DEV != False:
     from .settings_files.local import *
+
+    print("LOCAL SETTINGS USED")
 else:
     from .settings_files.aws import *
+
+    print("AWS SETTINGS USED")
