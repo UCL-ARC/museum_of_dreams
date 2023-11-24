35c9d6af3ae3786e460dab5b0b15caaf2733474b
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-24 13:26:48 +0000

    being explicit

--- museum_of_dreams_project/settings.py
+++ museum_of_dreams_project/settings.py
@@ -3,7 +3,7 @@ import os
 IS_LOCAL_DEV = os.environ.get("LOCAL_DEV", "False")
 print("IS_LOCAL_DEV", IS_LOCAL_DEV)
 
-if IS_LOCAL_DEV:
+if IS_LOCAL_DEV != False:
     from .settings_files.local import *
 else:
     from .settings_files.aws import *
