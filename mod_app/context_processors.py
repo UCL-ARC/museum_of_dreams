54bf7afb88c8daf7c7686d10891352f176b3fa96
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-30 16:52:05 +0000

    adding staging messages

---
+++ mod_app/context_processors.py
@@ -0,0 +1,20 @@
+# myapp/context_processors.py
+from django.contrib.sites.shortcuts import get_current_site
+
+
+def admin_custom_titles(request):
+    current_site = get_current_site(request)
+    site_header = "Administration for Museum of Dreamworlds Site"
+    site_title = "Museum of Dreamworlds Admin Site"
+    index_title = "Admin"
+
+    if current_site.domain.startswith("museumofdreams"):
+        site_header = "Administration for Museum of Dreams (staging)"
+        site_title = "Museum of Dreams Staging Admin Site"
+        index_title = "Admin"
+
+    return {
+        "admin_site_header": site_header,
+        "admin_site_title": site_title,
+        "admin_index_title": index_title,
+    }
--- museum_of_dreams_project/urls.py
+++ museum_of_dreams_project/urls.py
@@ -22,18 +22,12 @@ from django_distill import distill_path
 
 from mod_app import views
 
-admin.site.site_header = "Administration for Museum of Dreams"
-admin.site.site_title = "Museum of Dreams Admin Site"
-admin.site.index_title = "Admin"
 
 urlpatterns = [
     path("admin/", admin.site.urls, name=admin),
     distill_path("", views.HomeView.as_view(), name="home", distill_file="index.html"),
-
     distill_path("actors/list", views.ActorListView.as_view(), name="actor_list"),
     distill_path("actors/<pk>", views.ActorDetailView.as_view(), name="actor_detail"),
-
     distill_path("films/list", views.FilmListView.as_view(), name="film_list"),
     distill_path("films/<pk>", views.FilmDetailView.as_view(), name="film_detail"),
-    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
