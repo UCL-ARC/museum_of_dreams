8eb2156098c2dfcfd6b99c0a93d837e475710dac
acholyn <a.ho-lyn@ucl.ac.uk>
2023-12-01 09:27:58 +0000

    conditional admin titles

--- mod_app/middleware.py
+++ mod_app/middleware.py
@@ -1,4 +1,4 @@
-# myapp/context_processors.py
+from django.contrib import admin
 from django.contrib.sites.shortcuts import get_current_site
 
 
@@ -18,3 +18,18 @@ def admin_custom_titles(request):
         "admin_site_title": site_title,
         "admin_index_title": index_title,
     }
+
+
+class AdminTitlesMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        admin_titles = admin_custom_titles(request)
+        admin.site.site_header = admin_titles["admin_site_header"]
+        admin.site.site_title = admin_titles["admin_site_title"]
+        admin.site.index_title = admin_titles["admin_index_title"]
+
+        response = self.get_response(request)
+
+        return response
--- museum_of_dreams_project/settings/base.py
+++ museum_of_dreams_project/settings/base.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "mod_app.middleware.AdminTitlesMiddleware",
 ]
 
 ROOT_URLCONF = "museum_of_dreams_project.urls"
