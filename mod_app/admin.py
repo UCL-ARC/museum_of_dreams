fff2e9b4fcb0337dcd9a19aae6ea2450832193cd
acholyn <a.ho-lyn@ucl.ac.uk>
2023-12-14 14:45:32 +0000

    admin tweaks

--- mod_app/admin.py
+++ mod_app/admin.py
@@ -2,6 +2,7 @@ from ckeditor.widgets import CKEditorWidget
 from django import forms
 from django.contrib import admin
 from django.db import models
+from django.template.defaultfilters import truncatechars_html
 
 
 from .models import *
@@ -81,8 +82,8 @@ from django.utils.html import format_html
 @admin.register(Film)
 class FilmAdmin(admin.ModelAdmin):
     list_display = [
-        "video",
         "title",
+        "video",
         "alt_titles",
         "release_date",
         "production_country",
@@ -93,13 +94,15 @@ class FilmAdmin(admin.ModelAdmin):
 
     # these fields show html tags from the ckeditor otherwise
     def safe_synopsis(self, obj):
-        return format_html(obj.synopsis)
+        truncated_synopsis = truncatechars_html(obj.synopsis, 200)
+        return format_html(truncated_synopsis)
 
     safe_synopsis.allow_tags = True
     safe_synopsis.short_description = "Synopsis"
 
     def safe_comments(self, obj):
-        return format_html(obj.comments)
+        truncated_comments = truncatechars_html(obj.comments, 200)
+        return format_html(truncated_comments)
 
     safe_comments.allow_tags = True
     safe_comments.short_description = "Comments"
@@ -158,7 +161,7 @@ class FilmAdmin(admin.ModelAdmin):
             "Additional Information (non filmic)",
             {
                 "classes": ("grp-collapse grp-open",),
-                "fields": ("additional_links",),
+                # "fields": ("additional_links",),
             },
         ),
         (
@@ -191,7 +194,7 @@ class FilmAdmin(admin.ModelAdmin):
                 "classes": ("grp-collapse grp-open",),
                 "fields": (
                     "comments",
-                    # "temporary_images",
+                    "temporary_images",
                 ),
             },
         ),
