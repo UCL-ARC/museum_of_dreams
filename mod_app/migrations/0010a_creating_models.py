0b45ca2e7102f20d2eaf5b4b319b8ce41278e9bd
acholyn <a.ho-lyn@ucl.ac.uk>
2023-12-14 13:55:00 +0000

    disabling all except source model

--- mod_app/migrations/0010a_creating_models.py
+++ mod_app/migrations/0010a_creating_models.py
@@ -8,146 +8,146 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name="Drawing",
-            fields=[
-                (
-                    "filelink_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="mod_app.filelink",
-                    ),
-                ),
-            ],
-            options={
-                "verbose_name": "Drawing",
-            },
-            bases=("mod_app.filelink",),
-        ),
-        migrations.CreateModel(
-            name="Postcard",
-            fields=[
-                (
-                    "filelink_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="mod_app.filelink",
-                    ),
-                ),
-            ],
-            options={
-                "verbose_name": "Postcard",
-            },
-            bases=("mod_app.filelink",),
-        ),
-        migrations.CreateModel(
-            name="Poster",
-            fields=[
-                (
-                    "filelink_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="mod_app.filelink",
-                    ),
-                ),
-            ],
-            options={
-                "verbose_name": "Poster",
-            },
-            bases=("mod_app.filelink",),
-        ),
-        migrations.CreateModel(
-            name="PressBook",
-            fields=[
-                (
-                    "filelink_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="mod_app.filelink",
-                    ),
-                ),
-            ],
-            options={
-                "verbose_name": "Press Book",
-            },
-            bases=("mod_app.filelink",),
-        ),
-        migrations.CreateModel(
-            name="Programme",
-            fields=[
-                (
-                    "filelink_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="mod_app.filelink",
-                    ),
-                ),
-            ],
-            options={
-                "verbose_name": "Programme",
-            },
-            bases=("mod_app.filelink",),
-        ),
-        migrations.CreateModel(
-            name="Publicity",
-            fields=[
-                (
-                    "filelink_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="mod_app.filelink",
-                    ),
-                ),
-            ],
-            options={
-                "verbose_name": "Publicity Material",
-            },
-            bases=("mod_app.filelink",),
-        ),
-        migrations.CreateModel(
-            name="Script",
-            fields=[
-                (
-                    "filelink_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="mod_app.filelink",
-                    ),
-                ),
-            ],
-            options={
-                "verbose_name": "Script",
-            },
-            bases=("mod_app.filelink",),
-        ),
+        # migrations.CreateModel(
+        #     name="Drawing",
+        #     fields=[
+        #         (
+        #             "filelink_ptr",
+        #             models.OneToOneField(
+        #                 auto_created=True,
+        #                 on_delete=django.db.models.deletion.CASCADE,
+        #                 parent_link=True,
+        #                 primary_key=True,
+        #                 serialize=False,
+        #                 to="mod_app.filelink",
+        #             ),
+        #         ),
+        #     ],
+        #     options={
+        #         "verbose_name": "Drawing",
+        #     },
+        #     bases=("mod_app.filelink",),
+        # ),
+        # migrations.CreateModel(
+        #     name="Postcard",
+        #     fields=[
+        #         (
+        #             "filelink_ptr",
+        #             models.OneToOneField(
+        #                 auto_created=True,
+        #                 on_delete=django.db.models.deletion.CASCADE,
+        #                 parent_link=True,
+        #                 primary_key=True,
+        #                 serialize=False,
+        #                 to="mod_app.filelink",
+        #             ),
+        #         ),
+        #     ],
+        #     options={
+        #         "verbose_name": "Postcard",
+        #     },
+        #     bases=("mod_app.filelink",),
+        # ),
+        # migrations.CreateModel(
+        #     name="Poster",
+        #     fields=[
+        #         (
+        #             "filelink_ptr",
+        #             models.OneToOneField(
+        #                 auto_created=True,
+        #                 on_delete=django.db.models.deletion.CASCADE,
+        #                 parent_link=True,
+        #                 primary_key=True,
+        #                 serialize=False,
+        #                 to="mod_app.filelink",
+        #             ),
+        #         ),
+        #     ],
+        #     options={
+        #         "verbose_name": "Poster",
+        #     },
+        #     bases=("mod_app.filelink",),
+        # ),
+        # migrations.CreateModel(
+        #     name="PressBook",
+        #     fields=[
+        #         (
+        #             "filelink_ptr",
+        #             models.OneToOneField(
+        #                 auto_created=True,
+        #                 on_delete=django.db.models.deletion.CASCADE,
+        #                 parent_link=True,
+        #                 primary_key=True,
+        #                 serialize=False,
+        #                 to="mod_app.filelink",
+        #             ),
+        #         ),
+        #     ],
+        #     options={
+        #         "verbose_name": "Press Book",
+        #     },
+        #     bases=("mod_app.filelink",),
+        # ),
+        # migrations.CreateModel(
+        #     name="Programme",
+        #     fields=[
+        #         (
+        #             "filelink_ptr",
+        #             models.OneToOneField(
+        #                 auto_created=True,
+        #                 on_delete=django.db.models.deletion.CASCADE,
+        #                 parent_link=True,
+        #                 primary_key=True,
+        #                 serialize=False,
+        #                 to="mod_app.filelink",
+        #             ),
+        #         ),
+        #     ],
+        #     options={
+        #         "verbose_name": "Programme",
+        #     },
+        #     bases=("mod_app.filelink",),
+        # ),
+        # migrations.CreateModel(
+        #     name="Publicity",
+        #     fields=[
+        #         (
+        #             "filelink_ptr",
+        #             models.OneToOneField(
+        #                 auto_created=True,
+        #                 on_delete=django.db.models.deletion.CASCADE,
+        #                 parent_link=True,
+        #                 primary_key=True,
+        #                 serialize=False,
+        #                 to="mod_app.filelink",
+        #             ),
+        #         ),
+        #     ],
+        #     options={
+        #         "verbose_name": "Publicity Material",
+        #     },
+        #     bases=("mod_app.filelink",),
+        # ),
+        # migrations.CreateModel(
+        #     name="Script",
+        #     fields=[
+        #         (
+        #             "filelink_ptr",
+        #             models.OneToOneField(
+        #                 auto_created=True,
+        #                 on_delete=django.db.models.deletion.CASCADE,
+        #                 parent_link=True,
+        #                 primary_key=True,
+        #                 serialize=False,
+        #                 to="mod_app.filelink",
+        #             ),
+        #         ),
+        #     ],
+        #     options={
+        #         "verbose_name": "Script",
+        #     },
+        #     bases=("mod_app.filelink",),
+        # ),
         migrations.CreateModel(
             name="Source",
             fields=[
