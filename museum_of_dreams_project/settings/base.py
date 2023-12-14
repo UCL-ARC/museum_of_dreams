15e436c181dada13922069ee33b2b066d301cb6a
acholyn <a.ho-lyn@ucl.ac.uk>
2023-12-14 11:21:35 +0000

    enable ckeditor links/uploads

--- museum_of_dreams_project/settings/base.py
+++ museum_of_dreams_project/settings/base.py
@@ -110,6 +110,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
 
+CKEDITOR_UPLOAD_PATH = "media/editor/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
