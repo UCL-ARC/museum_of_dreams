593b575f81cabd5d9cf4601e2a1aae40c421f628
acholyn <a.ho-lyn@ucl.ac.uk>
2024-01-16 10:13:49 +0000

    applying improvement suggestions

--- mod_app/utils/extract_citations.py
+++ mod_app/utils/extract_citations.py
@@ -6,11 +6,8 @@ def update_bibliography(self, field):
     if field:
         bib_items = get_bibliography_items(field)
         # for each pk found add the bib item to the bibliography if it's changed
-        print("bib_list:", bib_items)
         if list(self.bibliography.all()) != bib_items:
-            self.bibliography.clear()
-            for bib in bib_items:
-                self.bibliography.add(bib)
+            self.bibliography.set(bib_items)
 
 
 def get_bibliography_items(field):
@@ -19,20 +16,7 @@ def get_bibliography_items(field):
 
     strong_tags = field_soup.find_all("strong", class_="bib-mention")
 
-    print("strong tags", strong_tags)
-    pk_list = []
-    for strong_tag in strong_tags:
-        pk = strong_tag.get("data-bib-id")
-        if pk:
-            print("yes pk")
-            pk_list.append(pk)
+    pk_list = [tag.get("data-bib-id") for tag in strong_tags]
+    bib_qs = BibliographyItem.objects.filter(pk__in=pk_list)
 
-    bib_list = []
-    for pk in pk_list:
-        try:
-            bib_item = BibliographyItem.objects.get(pk=pk)
-            bib_list.append(bib_item)
-        except (ValueError, IndexError, BibliographyItem.DoesNotExist) as e:
-            print(f"Error retrieving BibliographyItem {pk}: {e}")
-
-    return bib_list
+    return bib_qs
