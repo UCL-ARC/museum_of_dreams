2af787ce38c7ae8854e2c35c424cd676d977add2
acholyn <a.ho-lyn@ucl.ac.uk>
2024-01-31 15:19:46 +0000

    staticfiles storage var update

--- museum_of_dreams_project/settings/aws.py
+++ museum_of_dreams_project/settings/aws.py
@@ -29,7 +29,7 @@ AWS_QUERYSTRING_AUTH = False  # needed by grappelli to work with s3
 
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE = "storages.backends.s3.S3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
