5e0a30d0866222f2ccc8b60ff7c9d7e3e7a190be
acholyn <a.ho-lyn@ucl.ac.uk>
2024-01-31 16:07:02 +0000

    fixing error

--- museum_of_dreams_project/settings/aws.py
+++ museum_of_dreams_project/settings/aws.py
@@ -48,5 +48,5 @@ AWS_S3_REGION_NAME = "eu-west-2"
 AWS_S3_CUSTOM_DOMAIN = (
     f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 )
-STATIC_URL = "https://%s/static" % AWS_S3_CUSTOM_DOMAIN
-MEDIA_URL = "https://%s/media" % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = "https://%s/static/" % AWS_S3_CUSTOM_DOMAIN
+MEDIA_URL = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
