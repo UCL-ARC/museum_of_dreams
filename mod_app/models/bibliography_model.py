e849b94bbb262ac5de5c2b5241f774d1e38a738d
acholyn <a.ho-lyn@ucl.ac.uk>
2024-01-31 13:39:04 +0000

    updating help text for citations and comments

--- mod_app/models/bibliography_model.py
+++ mod_app/models/bibliography_model.py
@@ -10,5 +10,12 @@ class BibliographyItem(models.Model):
         plain_text = strip_tags(self.full_citation).replace("&nbsp;", " ")
         return f"\n{plain_text}"
 
-    short_citation = models.CharField(max_length=200, null=True)
-    full_citation = RichTextField(null=True)
+    short_citation = models.CharField(
+        max_length=200,
+        null=True,
+        help_text="Please use Harvard in-text citation style.                       Examples: (Author surname, year), or in the case of up to 3 authors: (Author 1 surname, Author 2 surname, Author 3 surname, year), or in the case or more than 3 authors: (Author 1 surname, et al., year) ",
+    )
+    full_citation = RichTextField(
+        null=True,
+        help_text="Please use Harvard reference list style. Examples: Author surname, initial. (year). 'Title of article/book/website/photograph'. Available at: URL or DOI where applicable (date accessed). Please note that the website or photo title should be italicised. For more detailed information and examples visit: https://www5.open.ac.uk/library/referencing-and-plagiarism/quick-guide-to-harvard-referencing-cite-them-right",
+    )
