9a7a47236b58f40ea295a24b1e854d5a52e9f461
acholyn <a.ho-lyn@ucl.ac.uk>
2024-01-31 16:52:33 +0000

    moving static.py

--- museum_of_dreams_project/static.py
+++ museum_of_dreams_project/static.py
@@ -1,5 +1,5 @@
 from storages.backends.s3boto3 import S3Boto3Storage
-from .aws import STATIC_ROOT, MEDIA_ROOT
+from .settings.aws import STATIC_ROOT, MEDIA_ROOT
 
 
 class StaticStorage(S3Boto3Storage):
