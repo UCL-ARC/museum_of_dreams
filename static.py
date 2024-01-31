7a3481010e57743e67448f40b156028d9d3f36af
acholyn <a.ho-lyn@ucl.ac.uk>
2024-01-31 17:07:58 +0000

    small path change

--- static.py
+++ static.py
@@ -1,5 +1,5 @@
 from storages.backends.s3boto3 import S3Boto3Storage
-from .museum_of_dreams_project.settings.aws import STATIC_ROOT, MEDIA_ROOT
+from museum_of_dreams_project.settings.aws import STATIC_ROOT, MEDIA_ROOT
 
 
 class StaticStorage(S3Boto3Storage):
