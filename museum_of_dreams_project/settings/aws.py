20986e3f90f24f066293975f0d87f771319fbfa5
acholyn <a.ho-lyn@ucl.ac.uk>
2024-01-31 16:14:50 +0000

    fixing path?

--- museum_of_dreams_project/settings/aws.py
+++ museum_of_dreams_project/settings/aws.py
@@ -29,8 +29,8 @@ GRAPPELLI_ADMIN_TITLE = "Museum of Dreamworlds"
 AWS_QUERYSTRING_AUTH = False  # needed by grappelli to work with s3
 
 
-DEFAULT_FILE_STORAGE = "static.MediaStorage"
-STATICFILES_STORAGE = "static.StaticStorage"
+DEFAULT_FILE_STORAGE = "settings.static.MediaStorage"
+STATICFILES_STORAGE = "settings.static.StaticStorage"
 
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
