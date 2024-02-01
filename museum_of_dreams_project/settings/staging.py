5cbbd3c0dbe4dfe8c937a076ad17e6f8350f6788
acholyn <a.ho-lyn@ucl.ac.uk>
2024-02-01 09:07:19 +0000

    copying params to staging

--- museum_of_dreams_project/settings/staging.py
+++ museum_of_dreams_project/settings/staging.py
@@ -8,3 +8,8 @@ ALLOWED_HOSTS = [
 ]
 GRAPPELLI_ADMIN_TITLE = "Museum of Dreams"
 AWS_STORAGE_BUCKET_NAME = "moddevbucket"
+AWS_S3_CUSTOM_DOMAIN = (
+    f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
+)
+STATIC_URL = "https://%s/static/" % AWS_S3_CUSTOM_DOMAIN
+MEDIA_URL = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
