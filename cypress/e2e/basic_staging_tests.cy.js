bee686e3645d3a3a4b94380db9d647d5ebc4c3b2
acholyn <a.ho-lyn@ucl.ac.uk>
2024-02-07 16:39:13 +0000

    basic cypress test

--- cypress/e2e/basic_staging_tests.cy.js
+++ cypress/e2e/basic_staging_tests.cy.js
@@ -1,7 +1,15 @@
 describe("general navigation", () => {
   it("navigates the staging website", () => {
-    cy.visit("https://staging.museumofdreamworlds.org/");
+    // cy.visit("http://museumofdreams.eu-west-2.elasticbeanstalk.com/"); // for whatever reason it times out when accessing the proper staging site
 
-    cy.contains("Museum of Dreams");
+    cy.visit("localhost:1080");
+
+    cy.contains("STAGING");
+
+    cy.get("#menu-icon").click();
+    cy.contains("Analyses: Critical Essays and Discussions").click();
+
+    cy.contains("Critical discussions and essays on this site");
+    cy.url().should("include", "/analyses");
   });
 });
