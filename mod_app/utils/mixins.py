aec59deb33ab73cd77b1263f94a28ea4d6bf8f48
acholyn <a.ho-lyn@ucl.ac.uk>
2024-03-26 15:09:08 +0000

    basic mixin

--- mod_app/utils/mixins.py
+++ mod_app/utils/mixins.py
@@ -1,4 +1,5 @@
 from django.utils.html import format_html
+from django.core.mail import send_mail
 
 
 class PreviewMixin:
@@ -25,3 +26,22 @@ class PreviewMixin:
             '<img src="{}" style="max-width: 15rem;" />',
             obj.url,
         )
+
+
+class EmailMixin:
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+        # Check if it's a new instance or an update
+        if change:
+            print(
+                "changed:", obj.__class__.__name__, request.path, obj.get_absolute_url()
+            )
+            # Send email notification for update
+            # send_mail(
+            #     f"{obj} Updated",
+            #     f'"{obj}" has been updated by {request.user}. Click ',
+            #     "sender@example.com",
+            #     ["recipient@example.com"],
+            #     fail_silently=False,
+            # )
