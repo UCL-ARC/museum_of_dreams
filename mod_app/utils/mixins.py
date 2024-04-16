96f02787ee453ac38d8d6762c9be1994909eed8a
acholyn <a.ho-lyn@ucl.ac.uk>
2024-04-16 11:13:16 +0100

    expanding mixin to identify deletion + creation

--- mod_app/utils/mixins.py
+++ mod_app/utils/mixins.py
@@ -33,30 +33,32 @@ class PreviewMixin:
 class EmailMixin:
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
+        updated_by = request.user.username
+        if updated_by == "acholyn":
+            return
 
+        researchers = User.objects.filter(groups__name="Researchers").exclude(
+            username=updated_by
+        )
+        recipients = [user.email for user in researchers]
+
+        if "staging" in request.build_absolute_uri():
+            website = "staging"
+        else:
+            website = "production"
+
+        instance_admin_url = request.build_absolute_uri()
+        instance_url = request.build_absolute_uri(obj.get_absolute_url())
         # Check if it's a new instance or an update
         if change:
-            updated_by = request.user.username
-            if updated_by == "acholyn":
-                return
-
-            researchers = User.objects.filter(groups__name="Researchers").exclude(
-                username=updated_by
-            )
-            recipients = [user.email for user in researchers]
             subject = f"A {obj.__class__.__name__} has been updated! || MOD"
 
-            if "staging" in request.build_absolute_uri():
-                website = "staging"
-            else:
-                website = "production"
-
-            instance_url = request.build_absolute_uri()
             html_message = render_to_string(
-                "email_template.html",
+                "email_template_update.html",
                 {
                     "updated_by": updated_by,
                     "instance": obj,
+                    "instance_admin_url": instance_admin_url,
                     "instance_url": instance_url,
                     "website": website,
                 },
@@ -69,3 +71,60 @@ class EmailMixin:
                 html_message=html_message,
                 fail_silently=False,
             )
+        else:
+            # new instance
+            subject = f"A {obj.__class__.__name__} has been added! || MOD"
+
+            html_message = render_to_string(
+                "email_template.html",
+                {
+                    "updated_by": updated_by,
+                    "instance": obj,
+                    "instance_admin_url": instance_admin_url,
+                    "instance_url": instance_url,
+                    "website": website,
+                },
+            )
+            send_mail(
+                subject,
+                "",
+                "notifications@museumofdreamworlds.org",
+                recipients,
+                html_message=html_message,
+                fail_silently=False,
+            )
+
+    def delete_model(self, request, obj):
+        updated_by = request.user.username
+        if updated_by == "acholyn":
+            return
+
+        researchers = User.objects.filter(groups__name="Researchers").exclude(
+            username=updated_by
+        )
+        recipients = [user.email for user in researchers]
+
+        if "staging" in request.build_absolute_uri():
+            website = "staging"
+        else:
+            website = "production"
+
+        subject = f"A {obj.__class__.__name__} has been deleted! || MOD"
+
+        html_message = render_to_string(
+            "email_template.html",
+            {
+                "updated_by": updated_by,
+                "instance": obj,
+                "website": website,
+            },
+        )
+        send_mail(
+            subject,
+            "",
+            "notifications@museumofdreamworlds.org",
+            recipients,
+            html_message=html_message,
+            fail_silently=False,
+        )
+        super().delete_model(request, obj)
