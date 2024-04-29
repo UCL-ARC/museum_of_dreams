f8b9f4b1d6d641fa6e7511d34282ad3685ca03d8
acholyn <a.ho-lyn@ucl.ac.uk>
2024-04-29 17:48:46 +0100

    abstracting and streamlining email function

--- mod_app/utils/mixins.py
+++ mod_app/utils/mixins.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import User
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
 from django.utils.html import format_html
+
+
+from .shared_functions import build_and_send_email
 
 
 class PreviewMixin:
@@ -33,103 +33,14 @@ class PreviewMixin:
 class EmailMixin:
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
-        updated_by = request.user.username
 
-        devs = [user.username for user in User.objects.filter(groups__name="devs")]
-
-        if updated_by in devs:
-            return
-
-        researchers = User.objects.filter(groups__name="Researchers").exclude(
-            username=updated_by
-        )
-        recipients = [user.email for user in researchers]
-
-        if "staging" in request.build_absolute_uri():
-            website = "staging"
-        else:
-            website = "production"
-
-        instance_admin_url = request.build_absolute_uri()
-        instance_url = request.build_absolute_uri(obj.get_absolute_url())
         # Check if it's a new instance or an update
         if change:
-            subject = f"A {obj.__class__.__name__} has been updated! || MOD"
-
-            html_message = render_to_string(
-                "email_template_update.html",
-                {
-                    "updated_by": updated_by,
-                    "instance": obj,
-                    "instance_admin_url": instance_admin_url,
-                    "instance_url": instance_url,
-                    "website": website,
-                },
-            )
-            send_mail(
-                subject,
-                "",
-                "notifications@museumofdreamworlds.org",
-                recipients,
-                html_message=html_message,
-                fail_silently=False,
-            )
+            build_and_send_email(request, obj, "updated")
         else:
             # new instance
-            subject = f"A {obj.__class__.__name__} has been added! || MOD"
-
-            html_message = render_to_string(
-                "email_template_new.html",
-                {
-                    "updated_by": updated_by,
-                    "instance": obj,
-                    "instance_admin_url": instance_admin_url,
-                    "instance_url": instance_url,
-                    "website": website,
-                },
-            )
-            send_mail(
-                subject,
-                "",
-                "notifications@museumofdreamworlds.org",
-                recipients,
-                html_message=html_message,
-                fail_silently=False,
-            )
+            build_and_send_email(request, obj, "added")
 
     def delete_model(self, request, obj):
-        updated_by = request.user.username
-        devs = [user.username for user in User.objects.filter(groups__name="devs")]
-
-        if updated_by in devs:
-            return
-
-        researchers = User.objects.filter(groups__name="Researchers").exclude(
-            username=updated_by
-        )
-        recipients = [user.email for user in researchers]
-
-        if "staging" in request.build_absolute_uri():
-            website = "staging"
-        else:
-            website = "production"
-
-        subject = f"A {obj.__class__.__name__} has been deleted! || MOD"
-
-        html_message = render_to_string(
-            "email_template_delete.html",
-            {
-                "updated_by": updated_by,
-                "instance": obj,
-                "website": website,
-            },
-        )
-        send_mail(
-            subject,
-            "",
-            "notifications@museumofdreamworlds.org",
-            recipients,
-            html_message=html_message,
-            fail_silently=False,
-        )
+        build_and_send_email(request, obj, "deleted")
         super().delete_model(request, obj)
