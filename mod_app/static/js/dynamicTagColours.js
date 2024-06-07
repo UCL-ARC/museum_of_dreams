36b373321600d6d0dc707c731bc400a37ddfefed
acholyn <a.ho-lyn@ucl.ac.uk>
2024-06-07 14:55:29 +0100

    fixing fonts and tags

--- mod_app/static/js/dynamicTagColours.js
+++ mod_app/static/js/dynamicTagColours.js
@@ -1,17 +1,22 @@
 // make the tag colours on the page semi random
 export function setTagColours() {
-	const tags = document.querySelectorAll(".tag");
-	const colors = [
-		"--camo-9",
-		"--bright-red",
-		"--tan",
-		"--violet-6",
-		"--blue-7",
-	];
-	tags.forEach((tag, index) => {
-		var dot = tag.querySelector(".dot");
-		tag.style.color = `var(${colors[index % colors.length]})`;
-		console.log("tag:", tag, "dot:", dot);
-		dot.style.color = `var(${colors[index % colors.length]})`;
-	});
+  const tags = document.querySelectorAll(".tag");
+  // when we know the tags, perhaps we assign the colours as fixed?
+  const colors = [
+    "--camo-9",
+    "--pink-10",
+    "--deep-blue",
+    "--violet-6",
+    "--blue-7",
+    "--teal-9",
+  ];
+  tags.forEach((tag, index) => {
+    tag.style.backgroundColor = `var(${colors[index % colors.length]})`;
+
+    // var dot = tag.querySelector(".dot");
+    // if (dot) {
+    //   console.log("tag:", tag, "dot:", dot);
+    //   dot.style.color = `var(${colors[index % colors.length]})`;
+    // }
+  });
 }
