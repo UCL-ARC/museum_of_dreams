96728c8e79a7d4a0f0729cebceac3321a2577b2f
acholyn <a.ho-lyn@ucl.ac.uk>
2024-12-09 13:35:23 +0000

    qol and aesthetic tweaks for new editor

--- mod_app/static/ckeditor/ckeditor/plugins/popout/plugin.js
+++ mod_app/static/ckeditor/ckeditor/plugins/popout/plugin.js
@@ -5,15 +5,37 @@ CKEDITOR.plugins.add("popout", {
     editor.addCommand("popout", {
       exec: function (editor) {
         var newWindow = window.open("", "_blank", "width=800,height=600");
-
+        // make sure new editor gets config
         var originalConfig = editor.config;
         var ckeditorBasePath = "/static/ckeditor/ckeditor/";
+
+        // styling and qol bits
         var btnStyle =
           "background-color: #23a1cc; border: none;border-radius: 4px; color: white; padding: 1rem 1.2rem; text-align: center; text-decoration: none; display: flex; font-size: 16px; margin: 0.8rem; cursor: pointer; justify-self: center";
 
+        var editorElement = editor.element.$;
+        var editorContainer = editorElement.closest(".c-2");
+        var labelElement =
+          editorContainer.previousElementSibling.querySelector("label"); // Find the label in the sibling 'c-1'
+        var labelText = labelElement
+          ? labelElement.innerText
+          : editor.name.split("id_").pop();
+
+        var instanceTitle = document.getElementById("id_title").value;
+        var path = window.location.pathname.split("mod_app/").pop();
+
+        // Remove the trailing "/change" if it exists
+        if (path.endsWith("/change")) {
+          path = path.slice(0, -7);
+        }
+        var windowTitle = instanceTitle || path;
+
         // html structure for new window
         newWindow.document.write(
-          `<html><head></head><body><textarea id="popout-editor"></textarea>          <button id="save-button" style="${btnStyle}">Save</button><button id="save-close-button" style="${btnStyle}">Save & Close</button></body></html>`
+          `<html><head>              <title>MOD Editor || ${windowTitle} </title>
+</head><body>
+<h3 style="font-family:sans-serif">Editing ${labelText} for ${instanceTitle}</h3><textarea id="popout-editor"></textarea>
+<button id="save-button" style="${btnStyle}">Save</button><button id="save-close-button" style="${btnStyle}">Save & Close</button></body></html>`
         );
 
         newWindow.document.close();
@@ -24,7 +46,6 @@ CKEDITOR.plugins.add("popout", {
         script.src = ckeditorBasePath + "ckeditor.js";
 
         script.onload = function () {
-          console.log(originalConfig);
           newWindow.CKEDITOR.replace("popout-editor", {
             ...originalConfig,
             // pass existing editor content to the new editor
