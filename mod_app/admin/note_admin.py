24b9768ef4f74f14ea0c93dc999d2b59420bcf4a
acholyn <a.ho-lyn@ucl.ac.uk>
2024-12-09 10:09:53 +0000

    adding vwi to admin and dashboard

--- mod_app/admin/note_admin.py
+++ mod_app/admin/note_admin.py
@@ -3,7 +3,7 @@ from django.template.defaultfilters import truncatechars_html
 from django.utils.html import format_html
 
 
-from mod_app.models import ProjectNote, Feedback
+from mod_app.models import ProjectNote, Feedback, VisualWrittenInfluences
 
 
 @admin.register(ProjectNote)
@@ -26,6 +26,24 @@ class ProjectNoteAdmin(admin.ModelAdmin):
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
+    class Media:
+        js = ("admin/js/mentionsPluginConfig.js",)
+
+    search_fields = ["title"]
+    list_display = ["title", "safe_content"]
+    readonly_fields = ("bibliography",)
+
+    def safe_content(self, obj):
+        truncated_content = truncatechars_html(obj.content, 200)
+        modified_content = truncated_content.replace("{", "(").replace("}", ")")
+        return format_html(modified_content)
+
+    safe_content.allow_tags = True
+    safe_content.short_description = "Content"
+
+
+@admin.register(VisualWrittenInfluences)
+class VWIAdmin(admin.ModelAdmin):
     class Media:
         js = ("admin/js/mentionsPluginConfig.js",)
 
