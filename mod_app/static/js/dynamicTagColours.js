e97ab26a4b498fb2d2cd337dfc121acba541a62a
acholyn <a.ho-lyn@ucl.ac.uk>
2025-03-18 15:17:12 +0000

    updating film tab content and styling

--- mod_app/static/js/dynamicTagColours.js
+++ mod_app/static/js/dynamicTagColours.js
@@ -1,6 +1,6 @@
 // make the tag colours on the page semi random
 export function setTagColours() {
-  const tags = document.querySelectorAll(".tag");
+  const tags = document.querySelectorAll(".columnar-area .tag");
   // when we know the tags, perhaps we assign the colours as fixed for consistency?
   const colors = ["--camo-9", "--pink-10", "--deep-blue", "--violet-6", "--blue-7", "--teal-9"];
   tags.forEach((tag, index) => {
