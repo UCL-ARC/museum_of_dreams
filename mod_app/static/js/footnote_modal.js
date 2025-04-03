a2034766d4b4a0326ab74f115b6998927b0bd691
shiying <shiying.wei@ucl.ac.uk>
2025-04-03 15:12:14 +0100

    Experiment with dialog

--- mod_app/static/js/footnote_modal.js
+++ mod_app/static/js/footnote_modal.js
@@ -4,37 +4,37 @@ const footnoteSection = document.querySelector("section.footnotes");
 
 // placeholder event handler code 
 document.addEventListener('DOMContentLoaded', function () {
-    const markers = document.querySelector(".tabbed-area").querySelectorAll("sup>a[rel='footnote']");
-    const modal = document.getElementById('referenceModal');
-    const modalText = document.getElementById('modalText');
-    const closeBtn = document.getElementById('closeModal');
+  const markers = document.querySelector(".tabbed-area").querySelectorAll("sup>a[rel='footnote']");
+  const modal = document.getElementById('referenceModal');
+  const modalText = document.getElementById('modalText');
+  const closeBtn = document.getElementById('closeModal');
 
-    markers.forEach((link) => {
-        link.addEventListener('click', function (e) {
-        e.preventDefault();
+  markers.forEach((link) => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
 
         let id = e.target.id;
-
+        
         // get the tag information of element that was clicked on
         const anchor = document.getElementById(id);
         // trace back to the href attribute (e.g., "#footnote-1")
         const footnoteId = anchor.getAttribute('href').substring(1); // removes '#'
-
+        
         // Find the footnote associated with that id ref
         const footnoteItem = document.getElementById(footnoteId);
-
+        
         // Get the citation text from the footnote
         const cite = footnoteItem.querySelector('cite');
-
+        
         modalText.innerHTML = cite.textContent;
         modal.style.display = 'block';
       });
     });
-  
+    
     closeBtn.onclick = function () {
       modal.style.display = 'none';
     };
-  
+    
     window.onclick = function (e) {
       if (e.target === modal) {
         modal.style.display = 'none';
@@ -42,3 +42,30 @@ document.addEventListener('DOMContentLoaded', function () {
     };
   });
 
+// dialog modal box
+// document.addEventListener('DOMContentLoaded',function(){
+//   const markers = document.querySelector(".tabbed-area").querySelectorAll("sup>a[rel='footnote']");
+//   const dialog = document.querySelector("dialog-modal");
+//   const modalText = document.getElementById('modalText');
+//   const closeButton = document.querySelector("dialog button");
+
+//   markers.forEach((link) => {
+//     link.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     let id = e.target.id;
+//     // get the tag information of element that was clicked on
+//     const anchor = document.getElementById(id);
+//     // trace back to the href attribute (e.g., "#footnote-1")
+//     const footnoteId = anchor.getAttribute('href').substring(1); // removes '#'
+//     // Find the footnote associated with that id ref
+//     const footnoteItem = document.getElementById(footnoteId);
+//     // Get the citation text from the footnote
+//     const cite = footnoteItem.querySelector('cite');
+//     modalText.innerHTML = cite.innerHTML;
+//     dialog.showModal();
+//   });
+//   closeButton.addEventListener("click", () => {
+//     dialog.close();
+//   });
+
+// });});
