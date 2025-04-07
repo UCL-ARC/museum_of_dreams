f4fdf0b281fecafde2e717fe0d71d916cc5a20ff
shiying <shiying.wei@ucl.ac.uk>
2025-04-07 16:11:30 +0100

    Show bibliography dialog on click

--- mod_app/static/js/footnote_modal.js
+++ mod_app/static/js/footnote_modal.js
@@ -5,8 +5,17 @@ const footnoteSection = document.querySelector("section.footnotes");
 document.addEventListener("DOMContentLoaded", function () {
   const markers = document.querySelector(".tabbed-area").querySelectorAll("sup>a[rel='footnote']");
   const referenceModal = document.getElementById("reference-modal");
-  const referenceModalText = referenceModal.querySelector("#modal__text");
-  const closeBtn = document.getElementById("modal__btn--close");
+  const referenceModalText = referenceModal.querySelector(".modal__text");
+
+  const bibModal = document.getElementById("bibliography-modal");
+  const bibModalContent = bibModal.querySelector(".modal__content");
+  const closeBtn = document.querySelectorAll(".modal__btn--close");
+
+  closeBtn.forEach((button) => {
+    button.onclick = function (e) {
+      e.target.parentNode.close();
+    };
+  });
 
   markers.forEach((link) => {
     link.addEventListener("click", function (e) {
@@ -27,44 +36,29 @@ document.addEventListener("DOMContentLoaded", function () {
       let cite = footnoteItem.querySelector("cite");
 
       referenceModalText.innerHTML = cite.innerHTML;
+      referenceModal.showModal();
 
-      // modal.style.display = "block";
-      // referenceModal.classList.add("visible");
-      referenceModal.showModal()
+      const mentions = document.querySelectorAll(".bib-mention");
+      mentions.forEach((mention) => {
+        mention.addEventListener("click", function (e) {
+          e.preventDefault();
+          const id = e.target.getAttribute("data-bib-id");
+          const bib = document.querySelector(`[data-bib-citation-id="${id}"]`);
+          bibModalContent.innerHTML = bib.innerHTML;
+          bibModal.showModal();
+        });
+      });
 
       closeBtn.onclick = function () {
-        referenceModal.close()
+        e.target.parentNode.close();
       };
 
       //window.onclick = function (e) {
-        // todo: make into if not modal clicked, close modal?
-        // if (e.target === referenceModal) {
-        //   modal.style.display = "none";
-        // }
+      // todo: make into if not modal clicked, close modal?
+      // if (e.target === referenceModal) {
+      //   modal.style.display = "none";
+      // }
       //};
     });
-  });
-});
-
-const mentions = document.querySelectorAll(".bib-mention");
-console.log("mentions", mentions);
-const bibModal = document.getElementById("bibliography-modal");
-const bibModalContent = bibModal.querySelector(".modal__content");
-const closeButton = document.getElementById("modal__btn--close");
-
-closeButton.addEventListener("click", () => {
-  dialog.close();
-});
-
-mentions.forEach((mention) => {
-  mention.addEventListener("click", function (e) {
-    e.preventDefault();
-    console.log(e)
-    const id = e.target.getAttribute("data-bib-id");
-    console.log("data-bib-id", id);
-    const bib = document.querySelector(`[data-bib-citation-id="${id}"]`);
-    bibModalContent.innerHTML = bib.innerHTML;
-    // bibModal.showModal();
-    
   });
 });
