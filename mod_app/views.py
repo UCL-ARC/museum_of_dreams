1bca152aab8e1e91bbc38b4418fb7346438d604c
shiying <shiying.wei@ucl.ac.uk>
2025-04-11 11:25:22 +0100

    Remove unused imports

--- mod_app/views.py
+++ mod_app/views.py
@@ -18,7 +18,7 @@ from museum_of_dreams_project.settings.aws import (
     AWS_STORAGE_BUCKET_NAME,
 )
 
-from .models import Analysis, BibliographyItem, Film, Keyword, Tag, TeachingResources
+from .models import Analysis, BibliographyItem, Film, Tag, TeachingResources
 
 
 class HomeView(TemplateView):
