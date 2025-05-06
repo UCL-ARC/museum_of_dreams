ab4830291baa3bebd52f3812b2144b403ab05846
shiying <shiying.wei@ucl.ac.uk>
2025-05-06 18:28:48 +0100

    Builds template for production and staging stacks

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 import aws_cdk as cdk
+from stacks.production_stack import ProductionStack
+from stacks.staging_stack import StagingStack
 from stacks.vpc_stack import VPCStack
 from stacks.database_stack import DatabaseStack
 
 app = cdk.App()
 
 vpc_stack = VPCStack(app, "CdkVPCStack")
-
 database_stack = DatabaseStack(app, "CdkDatabaseStack", vpc=vpc_stack.vpc)
+staging_stack = StagingStack(app, "StagingStack")
+production_stack = ProductionStack(app, "ProductionStack")
 
 app.synth()
