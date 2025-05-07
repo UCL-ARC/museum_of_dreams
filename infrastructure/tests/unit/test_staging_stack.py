559c8790c884b88c3677d6cafad35b6628d55090
shiying <shiying.wei@ucl.ac.uk>
2025-05-07 16:49:48 +0100

    Rename file

--- infrastructure/tests/unit/test_staging_stack.py
+++ infrastructure/tests/unit/test_staging_stack.py
@@ -1,15 +1,16 @@
-import aws_cdk as core
+import aws_cdk as cdk
 import aws_cdk.assertions as assertions
 
-from stacks.production_stack import ProductionStack
+from stacks.staging_stack import StagingStack
 
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in infrastructure/infrastructure_stack.py
 def test_sqs_queue_created():
-    app = core.App()
-    stack = ProductionStack(app, "ProductionStack")
+    app = cdk.App()
+    stack = StagingStack(app, "StagingStack")
     template = assertions.Template.from_stack(stack)
+    # template.has_resource("AWS::ElasticBeanstalk::Application",)
 
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
