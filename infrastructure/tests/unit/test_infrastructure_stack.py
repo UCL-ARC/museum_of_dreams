dbf64ab318095548fa23980d46884cccc78eeae0
shiying <shiying.wei@ucl.ac.uk>
2025-05-07 10:15:05 +0100

    Rename test stack

--- infrastructure/tests/unit/test_infrastructure_stack.py
+++ infrastructure/tests/unit/test_infrastructure_stack.py
@@ -1,14 +1,16 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from infrastructure.infrastructure_stack import InfrastructureStack
+from stacks.production_stack import ProductionStack
+
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in infrastructure/infrastructure_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = InfrastructureStack(app, "infrastructure")
+    stack = ProductionStack(app, "ProductionStack")
     template = assertions.Template.from_stack(stack)
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
