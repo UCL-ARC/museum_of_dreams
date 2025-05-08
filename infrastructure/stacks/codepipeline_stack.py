6bdc0f493fa6607dfaac7daf9331981ce298d2f0
shiying <shiying.wei@ucl.ac.uk>
2025-05-08 16:51:03 +0100

    Add deploy stage to pipeline

--- infrastructure/stacks/codepipeline_stack.py
+++ infrastructure/stacks/codepipeline_stack.py
@@ -1,11 +1,10 @@
-import aws_cdk as cdk
 from aws_cdk import (
     Stack,
     aws_codepipeline as codepipeline,
     aws_s3 as s3,
     aws_codebuild as codebuild,
     aws_codepipeline_actions as cpactions,
-    aws_ssm as ssm,
+    aws_iam as iam,
 )
 from constructs import Construct
 
@@ -14,7 +13,7 @@ class CodePipelineStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        artifact_bucket = s3.Buckect(self, "ArtifactBucket")
+        artifact_bucket = s3.Bucket(self, "ArtifactBucket")
         source_output = codepipeline.Artifact()
         build_output = codepipeline.Artifact()
         build_spec = codebuild.BuildSpec.from_source_filename("buildspec.yaml")
@@ -33,15 +32,14 @@ class CodePipelineStack(Stack):
         pipeline.add_stage(
             stage_name="Source",
             actions=[
-                cpactions.GitHubSourceAction(
+                cpactions.CodeStarConnectionsSourceAction(
                     action_name="GitHub_Source",
                     owner="UCL-ARC",
                     repo="museum_of_dreams",
                     branch="feature/cdk-codepipeline",
                     output=source_output,
-                    connection_arn=ssm.StringParameter.value_for_string_parameter(
-                        self, "/pipeline/github-connection-arn"
-                    ),  # needs to setup a parameter first in system manager
+                    connection_arn="",
+                    # needs to setup a parameter first in system manager
                 )
             ],
         )
@@ -49,12 +47,39 @@ class CodePipelineStack(Stack):
         # Build stage
         pipeline.add_stage(
             stage_name="Build",
-            action=[
+            actions=[
                 cpactions.CodeBuildAction(
                     action_name="CodeBuild",
                     project=build_project,
                     input=source_output,
                     outputs=[build_output],
+                )
+            ],
+        )
+
+        # Grant Permission via IAM Role to Pipeline for Elastic Beanstalk Deployment
+
+        eb_deploy_role = iam.Role(
+            self,
+            "EBDeployRole",
+            assumed_by=iam.ServicePrincipal("codepipeline.amazonzws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "AWSElasticBeanstalkFullAccess"
+                )  # to be limited
+            ],
+        )
+
+        # Deploy stage
+
+        pipeline.add_stage(
+            stage_name="Deploy",
+            actions=[
+                cpactions.ElasticBeanstalkDeployAction(
+                    action_name="DeployToElasticBeanstalk",
+                    application_name="",
+                    environment_name="",
+                    input=build_output,
                 )
             ],
         )
