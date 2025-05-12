0bebadfefdf32647e627ece603401a0c56d955a9
shiying <shiying.wei@ucl.ac.uk>
2025-05-12 14:44:53 +0100

    Remove build stage since it adds complexity to the CI process

--- infrastructure/stacks/codepipeline_stack.py
+++ infrastructure/stacks/codepipeline_stack.py
@@ -2,33 +2,34 @@ from aws_cdk import (
     Stack,
     aws_codepipeline as codepipeline,
     aws_s3 as s3,
-    aws_codebuild as codebuild,
     aws_codepipeline_actions as cpactions,
     aws_iam as iam,
+    aws_ssm as ssm,
 )
+
 from constructs import Construct
 
 
 class CodePipelineStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        staging_app_name,
+        staging_env_name,
+        **kwargs,
+    ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         artifact_bucket = s3.Bucket(self, "ArtifactBucket")
         source_output = codepipeline.Artifact()
-        build_output = codepipeline.Artifact()
-        build_spec = codebuild.BuildSpec.from_source_filename("buildspec.yaml")
-
-        # Code build project, can be customised with env var, buildspec, ect.
-        build_project = codebuild.PipelineProject(
-            self, "CdkProject", build_spec=build_spec
-        )
 
         # Pipeline
         pipeline = codepipeline.Pipeline(
             self, "CdkPipeline", artifact_bucket=artifact_bucket
         )
 
-        # Source stage - configured different for production/staging/dev branches
+        # Source stage - to be configured differently for production/staging/dev branches
         pipeline.add_stage(
             stage_name="Source",
             actions=[
@@ -38,21 +39,9 @@ class CodePipelineStack(Stack):
                     repo="museum_of_dreams",
                     branch="feature/cdk-codepipeline",
                     output=source_output,
-                    connection_arn="",
-                    # needs to setup a parameter first in system manager
-                )
-            ],
-        )
-
-        # Build stage
-        pipeline.add_stage(
-            stage_name="Build",
-            actions=[
-                cpactions.CodeBuildAction(
-                    action_name="CodeBuild",
-                    project=build_project,
-                    input=source_output,
-                    outputs=[build_output],
+                    connection_arn=ssm.StringParameter.value_for_string_parameter(
+                        self, "/pipeline/github-connection-arn"
+                    ),
                 )
             ],
         )
@@ -65,8 +54,11 @@ class CodePipelineStack(Stack):
             assumed_by=iam.ServicePrincipal("codepipeline.amazonzws.com"),
             managed_policies=[
                 iam.ManagedPolicy.from_aws_managed_policy_name(
-                    "AWSElasticBeanstalkFullAccess"
-                )  # to be limited
+                    "AWSElasticBeanstalkManagedUpdatesCustomerRolePolicy"
+                ),
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "CloudWatchLogsFullAccess"
+                ),
             ],
         )
 
@@ -77,9 +69,9 @@ class CodePipelineStack(Stack):
             actions=[
                 cpactions.ElasticBeanstalkDeployAction(
                     action_name="DeployToElasticBeanstalk",
-                    application_name="",
-                    environment_name="",
-                    input=build_output,
+                    application_name=staging_app_name,
+                    environment_name=staging_env_name,
+                    input=source_output,
                 )
             ],
         )
