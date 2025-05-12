c0c920f25af03e69f73a9797c0309086510f6fe2
shiying <shiying.wei@ucl.ac.uk>
2025-05-12 20:30:57 +0100

    Setup an SQL database instance with rds stack

--- infrastructure/stacks/database_stack.py
+++ infrastructure/stacks/database_stack.py
@@ -1,6 +1,4 @@
-from aws_cdk import (
-    aws_ec2 as ec2,
-)
+from aws_cdk import aws_ec2 as ec2, aws_rds as rds, RemovalPolicy
 
 from constructs import Construct
 
@@ -13,3 +11,25 @@ class DatabaseStack:
 
         # Security group for RDS
         rds_sg = ec2.SecurityGroup(self, "RDSInstanceSG", vpc=env_vpc)
+
+        rds.DatabaseInstance(
+            self,
+            "CdkSQLDatabase",
+            engine=rds.DatabaseInstanceEngine.mysql(
+                "version=rds.MysqlEngineVersion.VER_8_0_34"
+            ),
+            credentials=rds.Credentials.from_generated_secret("admin"),
+            vpc=env_vpc,
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO
+            ),
+            allocated_storage=20,
+            vpc_subnets=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
+            ),
+            security_groups=[rds_sg],
+            publicly_accessible=False,
+            removal_policy=RemovalPolicy.DESTROY,
+            deletion_protection=False,
+            multi_az=False,
+        )
