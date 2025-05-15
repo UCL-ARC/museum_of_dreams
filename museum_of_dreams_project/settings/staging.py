64bcba2f34cb0d8d9dfd4afb8d0417955422a33d
shiying <shiying.wei@ucl.ac.uk>
2025-05-15 16:52:50 +0100

    Revert "Add database for staging settings"
    This reverts commit 7c0de2a871a3f0a26e1301c153f2c552c50dc024.

--- museum_of_dreams_project/settings/staging.py
+++ museum_of_dreams_project/settings/staging.py
@@ -1,5 +1,4 @@
 from .aws import LOCAL_IP, AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME
-import os
 
 ENVIRONMENT = "staging"
 ALLOWED_HOSTS = [
@@ -12,16 +11,5 @@ GRAPPELLI_ADMIN_TITLE = "Museum of Dreams"
 AWS_S3_CUSTOM_DOMAIN = (
     f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 )
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("RDS_DB_NAME", "none"),
-        "USER": os.environ.get("RDS_USERNAME", "none"),
-        "PASSWORD": os.environ.get("RDS_PASSWORD", "none"),
-        "HOST": os.environ.get("RDS_HOSTNAME", "none"),
-        "PORT": os.environ.get("RDS_PORT", "none"),
-    }
-}
-
 STATIC_URL = "https://%s/static/" % AWS_S3_CUSTOM_DOMAIN
 MEDIA_URL = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
