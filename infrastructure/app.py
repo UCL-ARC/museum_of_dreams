65a17d98e1e9168c562040229d43490f19540471
shiying <shiying.wei@ucl.ac.uk>
2025-05-22 14:58:59 +0100

    Rename stack name and file name for clarity

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -2,11 +2,11 @@
 
 import aws_cdk as cdk
 from stacks.vpc_stack import VPCStack
-from stacks.database_stack import DatabaseStack
+from stacks.staging_database_stack import StagingDatabaseStack
 
 app = cdk.App()
 
 vpc_stack = VPCStack(app, "VPCStack")
-database_stack = DatabaseStack(app, "StagingDbStack", vpc=vpc_stack.vpc)
+database_stack = StagingDatabaseStack(app, "StagingDbStack", vpc=vpc_stack.vpc)
 
 app.synth()
