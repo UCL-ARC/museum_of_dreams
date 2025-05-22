20d6ee34a79cde29f4499de6414fdbbd9b5d091b
shiying <shiying.wei@ucl.ac.uk>
2025-05-22 12:32:24 +0100

    Create an Elastic Beanstalk App, environment instance, as well as Iam role, instance profile that can be assigned to the app & env

--- infrastructure/stacks/staging_stack.py
+++ infrastructure/stacks/staging_stack.py
@@ -1,9 +1,38 @@
 from aws_cdk import (
     Stack,
+    aws_ec2 as ec2,
+    aws_elasticbeanstalk as eb,
+    aws_iam as iam,
 )
+
 from constructs import Construct
 
 
 class StagingStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # bucket: s3.Bucket()
+        # pipeline: CfnPipeline, CfnWebhook
+
+        # ELastic beanstalk
+
+        # App
+        eb_app = eb.CfnApplication(self, "Staging", application_name="Staging")
+
+        # permissions
+        eb_role = iam.Role(
+            self, "EBRole", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com")
+        )
+        eb_profile = iam.CfnInstanceProfile(
+            self, "InstanceProfile", roles=[eb_role.role_name]
+        )
+
+        # Environment creation
+        eb_env = eb.CfnEnvironment(
+            self,
+            "MODStagingEnv",
+            environment_name="MODStagingEnv",
+            application_name=eb_app.application_name,
+            solution_stack_name="64bit Amazon Linux 2023/4.0.9 running Python 3.11",
+        )
