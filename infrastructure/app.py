9ce548387fd74bcde99a52f99053987dd04d49a9
shiying <shiying.wei@ucl.ac.uk>
2025-05-23 10:57:21 +0100

    Add description for cloudformation stacks

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -6,7 +6,16 @@ from stacks.staging_database_stack import StagingDatabaseStack
 
 app = cdk.App()
 
-vpc_stack = VPCStack(app, "VPCStack")
-database_stack = StagingDatabaseStack(app, "StagingDbStack", vpc=vpc_stack.vpc)
+vpc_stack = VPCStack(
+    app,
+    "VPCStack",
+    description="Stack for VPC, sets up a VPC network that is used in RDS & EB stacks",
+)
+database_stack = StagingDatabaseStack(
+    app,
+    "StagingDbStack",
+    description="Stack for staging database(can be deleted), also contains relevant security groups for RDS & EB",
+    vpc=vpc_stack.vpc,
+)
 
 app.synth()
