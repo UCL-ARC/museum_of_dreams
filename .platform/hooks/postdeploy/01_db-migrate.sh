e277d36085eb0d6f1c2d66b232817bd265250032
shiying <shiying.wei@ucl.ac.uk>
2025-06-02 16:06:26 +0100

    Revert "Update post deploy script to avoid failing pipeline"
    This reverts commit 22bf193ea5595a576c6b9e0a32f4c4e4e56549b9.

--- .platform/hooks/postdeploy/01_db-migrate.sh
+++ .platform/hooks/postdeploy/01_db-migrate.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-set -e  # exit on unhandled errors by default
-
-log() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
-}
-
-fail_safe_run() {
-    "$@" || log "WARNING: Command '$*' failed, continuing..."
-}
-
 if [ -f /var/app/current/manage.py ]; then
     # Start venv
     cd /var/app
@@ -17,14 +6,12 @@ if [ -f /var/app/current/manage.py ]; then
     cd current
 
     # Run migrations
-    fail_safe_run manage.py migrate
-
+    python manage.py migrate
     echo `date`
-    
-    fail_safe_run python manage.py showmigrations
+    echo `python manage.py showmigrations`
 
     # Collect static files
-    fail_safe_run python manage.py collectstatic --no-input --verbosity 0
+    #python manage.py collectstatic --no-input --verbosity 0
 else
     echo "manage.py not found."
     exit 1
