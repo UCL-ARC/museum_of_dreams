c4dcf15fb6c731df3a0bc20347b00e81832b2c86
shiying <shiying.wei@ucl.ac.uk>
2025-06-02 16:07:24 +0100

    Revert "debugging collectstatic"
    This reverts commit 4acef6031258a314517259325658cbb4f145e6fd.

--- .platform/hooks/postdeploy/01_db-migrate.sh
+++ .platform/hooks/postdeploy/01_db-migrate.sh
@@ -11,7 +11,7 @@ if [ -f /var/app/current/manage.py ]; then
     echo `python manage.py showmigrations`
 
     # Collect static files
-    #python manage.py collectstatic --no-input --verbosity 0
+    python manage.py collectstatic --no-input --verbosity 0
 else
     echo "manage.py not found."
     exit 1
