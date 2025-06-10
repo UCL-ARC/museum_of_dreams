55de0a2f43eec3a9e80f7d427e8301b5130f99fd
shiying <shiying.wei@ucl.ac.uk>
2025-06-10 18:37:08 +0100

    Fix and verifies if migration are correctly applied in tests

--- mod_app/tests/test_migrations/test_0040.py
+++ mod_app/tests/test_migrations/test_0040.py
@@ -1,5 +1,6 @@
 from django.db import connection
 from django.db.migrations.executor import MigrationExecutor
+from django.db.migrations.recorder import MigrationRecorder
 from django.test import TransactionTestCase
 
 
@@ -10,26 +11,50 @@ class TestUserCopyMigration(TransactionTestCase):
         self.executor = MigrationExecutor(connection)
 
     def migrate_to(self, migration_name):
+        # The MigrationExecutor caches the migration graph and states. If you migrate to a certain state (e.g. 0002_initial), and then later try to migrate to 0003_add_field, Django may think it has already applied it — especially if the migration record exists in the database, or if the executor wasn't reloaded.
+        self.executor = MigrationExecutor(
+            connection
+        )  # Reloads the executor each time function is called, to prevent migrations not applying
         self.executor.migrate([(self.app, migration_name)])
         return self.executor.loader.project_state((self.app, migration_name)).apps
+
+    def is_migration_applied(self, app, migration):
+        recorder = MigrationRecorder(connection)
+
+        applied = recorder.applied_migrations()
+        return (app, migration) in applied
 
     def test_transfer_keyword_to_topic(self):
         # Migrate to state before the transfer
         mod_app_pre_migration = self.migrate_to("0039_add_topic_field_to_Film")
+
+        self.assertTrue(
+            self.is_migration_applied("mod_app", "0039_add_topic_field_to_Film")
+        )
+
         KeywordPreMigration = mod_app_pre_migration.get_model("mod_app", "Keyword")
 
         # Create testcases in the pre-migration model
         KeywordPreMigration.objects.create(name="Keyword1", is_genre=False)
         KeywordPreMigration.objects.create(name="Keyword2", is_genre=True)
 
-        # Migrate to the state after the transfer
+        self.assertEqual(KeywordPreMigration.objects.count(), 2)
+
+        # Migrate to the state after the transfer,
         mod_app_post_migration = self.migrate_to(
             "0040_data_migration_transfer_keyword_to_topic"
         )
+
+        self.assertTrue(
+            self.is_migration_applied(
+                "mod_app", "0040_data_migration_transfer_keyword_to_topic"
+            )
+        )
+
         TopicPostMigration = mod_app_post_migration.get_model("mod_app", "Topic")
         KeywordPostMigration = mod_app_post_migration.get_model("mod_app", "Keyword")
 
-        # Assert Keywords was deleted
+        # Assert Keywords are deleted
         self.assertFalse(
             KeywordPostMigration.objects.filter(
                 name="Keyword1", is_genre=False
