2b8c6b4119a96a4e380b2018dcc747bd985190c8
shiying <shiying.wei@ucl.ac.uk>
2025-06-17 15:06:22 +0100

    Refactor tests into subtests

--- mod_app/tests/test_model_relationship.py
+++ mod_app/tests/test_model_relationship.py
@@ -12,7 +12,7 @@ from mod_app.models import (
     Topic,
     Video,
     VisualInfluences,
-    WrittenInfluences,
+    OtherLink,
 )
 
 
@@ -24,7 +24,9 @@ class RelationshipFixtureTests(TestCase):
         cls.analysis = Analysis.objects.create(title="Test Analysis")
         cls.tr = TeachingResources.objects.create(title="Test Resource")
         cls.vi = VisualInfluences.objects.create(title="Test VisualInfluences")
-        cls.wi = WrittenInfluences.objects.create(title="Test WrittenInfluences")
+        cls.baselinkmodel = OtherLink.objects.create(
+            description="Test BaseLinkModel"
+        )  # testing on child model object because BaseLinkModel is abstract
         cls.source = Source.objects.create(description="Test Source")
 
         cls.tag = Tag.objects.create(name="Tag1", is_genre=False)
@@ -37,62 +39,61 @@ class RelationshipFixtureTests(TestCase):
         cls.clip = Video.objects.create(description="Clip1")
         cls.archive = Archive.objects.create(name="Archive1")
 
-    def test_film_keyword(self):
-        self.film.keyword.add(self.keyword)
-        self.assertIn(self.keyword, self.film.keyword.all())
+        cls.film_relationships = {
+            "keyword": cls.keyword,
+            "topic": cls.topic,
+            "genre": cls.genre,
+            "bibliography": cls.bibliography,
+        }
 
-    def test_film_topic(self):
-        self.film.topic.add(self.topic)
-        self.assertIn(self.topic, self.film.topic.all())
+        cls.analysis_relationships = {
+            "films": cls.film,
+            "topics": cls.topic,
+            "keywords": cls.keyword,
+            "genre": cls.genre,
+            "bibliography": cls.bibliography,
+        }
 
-    def test_film_genre(self):
-        self.film.genre.add(self.genre)
-        self.assertIn(self.genre, self.film.genre.all())
+        cls.tr_relationships = {
+            "tags": cls.tag,
+            "clips": cls.clip,
+        }
 
-    def test_film_bibliography(self):
-        self.film.bibliography.add(self.bibliography)
-        self.assertIn(self.bibliography, self.film.bibliography.all())
+        cls.vi_relationships = {
+            "films": cls.film,
+            "archive": cls.archive,
+            "bibliography": cls.bibliography,
+        }
 
-    def test_analysis_films(self):
-        self.analysis.films.add(self.film)
-        self.assertIn(self.film, self.analysis.films.all())
+    def test_film_relationships(self):
+        for field_name, related_object in self.film_relationships.items():
+            # separate loop into individual subtests which outputs distinct fail reports
+            with self.subTest(field=field_name):
+                related_manager = getattr(self.film, field_name)
+                related_manager.add(related_object)
+                self.assertIn(related_object, related_manager.all())
 
-    def test_analysis_topics(self):
-        self.analysis.topics.add(self.topic)
-        self.assertIn(self.topic, self.analysis.topics.all())
+    def test_analysis_relationships(self):
+        for field_name, related_object in self.analysis_relationships.items():
+            with self.subTest(field=field_name):
+                related_manager = getattr(self.analysis, field_name)
+                related_manager.add(related_object)
+                self.assertIn(related_object, related_manager.all())
 
-    def test_analysis_keywords(self):
-        self.analysis.keywords.add(self.keyword)
-        self.assertIn(self.keyword, self.analysis.keywords.all())
+    def test_tr_relationships(self):
+        for field_name, related_object in self.tr_relationships.items():
+            with self.subTest(msg="test", field=field_name):
+                related_manager = getattr(self.tr, field_name)
+                related_manager.add(related_object)
+                self.assertIn(related_object, related_manager.all())
 
-    def test_analysis_genre(self):
-        self.analysis.genre.add(self.genre)
-        self.assertIn(self.genre, self.analysis.genre.all())
+    def test_vi_relationships(self):
+        for field_name, related_object in self.vi_relationships.items():
+            with self.subTest(field=field_name):
+                related_manager = getattr(self.vi, field_name)
+                related_manager.add(related_object)
+                self.assertIn(related_object, related_manager.all())
 
-    def test_analysis_teaching_resources(self):
-        self.analysis.teaching_resources.add(self.tr)
-        self.assertIn(self.tr, self.analysis.teaching_resources.all())
-
-    def test_analysis_bibliography(self):
-        self.analysis.bibliography.add(self.bibliography)
-        self.assertIn(self.bibliography, self.analysis.bibliography.all())
-
-    def test_teachingresources_clips(self):
-        self.tr.clips.add(self.clip)
-        self.assertIn(self.clip, self.tr.clips.all())
-
-    def test_teachingresources_tags(self):
-        self.tr.tags.add(self.tag)
-        self.assertIn(self.tag, self.tr.tags.all())
-
-    def test_visualinfluences_archive(self):
-        self.vi.archive.add(self.archive)
-        self.assertIn(self.archive, self.vi.archive.all())
-
-    def test_writteninfluences_archive(self):
-        self.wi.archive.add(self.archive)
-        self.assertIn(self.archive, self.wi.archive.all())
-
-    def test_source_film(self):
+    def test_source_relationship(self):
         self.source.film.add(self.film)
         self.assertIn(self.film, self.source.film.all())
