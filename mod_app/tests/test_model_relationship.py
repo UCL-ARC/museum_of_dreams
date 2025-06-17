c33d44a5f4ba24f23fde2dc6ca1947beb205c78b
shiying <shiying.wei@ucl.ac.uk>
2025-06-17 11:39:35 +0100

    Add test, model imports

---
+++ mod_app/tests/test_model_relationship.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from models import (
+    Film,
+    Analysis,
+    TeachingResources,
+    VisualInfluences,
+    WrittenInfluences,
+    BaseLinkModel,
+    FileLink,
+    Source,
+    Keyword,
+    Topic,
+    Genre,
+    Bibliography,
+    Tag,
+    Clip,
+    Archive,
+)
