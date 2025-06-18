8e5c5c89f6dd11ae503515f37afcf5ab6a8c37da
shiying <shiying.wei@ucl.ac.uk>
2025-06-18 14:16:23 +0100

    Setup fixtures correctly and update to class-wide for persistent use in test

--- mod_app/tests/test_views/test_view_context_data.py
+++ mod_app/tests/test_views/test_view_context_data.py
@@ -4,26 +4,28 @@ from mod_app.models import Film, Analysis, TeachingResources, Tag
 
 
 class TestViewContextData(TestCase):
-    def Setup(self):
-        self.client = Client()
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+        cls.film = Film.objects.create(title="Test Film", release_date="2020")
+        cls.analysis = Analysis.objects.create(title="Test Analysis")
+        cls.tr = TeachingResources.objects.create(title="Test TeachingResources")
+        cls.tag = Tag.objects.create(name="Test Tag")
 
     def test_homeview_context(self):
         response = self.client.get(reverse("home"))
 
     def test_film_detailview_context(self):
-        response = self.client.get(reverse("film_detail"))
+        response = self.client.get(reverse("film_detail", args=[self.film.pk]))
 
     def test_analysis_detailview_context(self):
-        analysis = Analysis.objects.create(title="Test Analysis")
-        response = self.client.get(reverse("analysis_detail"))
-        self.assertIn(analysis, response.context)
-        self.assertEqual(response.context[analysis], "expected_value")
+        response = self.client.get(reverse("analysis_detail", args=[self.analysis.pk]))
 
     def test_tr_detailview_context(self):
-        response = self.client.get(reverse("tr_detail"))
+        response = self.client.get(reverse("tr_detail", args=[self.tr.pk]))
 
     def test_tag_listview_context(self):
         response = self.client.get(reverse("tag_list"))
 
     def test_tag_detailview_context(self):
-        response = self.client.get(reverse("tag_detail"))
+        response = self.client.get(reverse("tag_detail", args=[self.tag.pk]))
