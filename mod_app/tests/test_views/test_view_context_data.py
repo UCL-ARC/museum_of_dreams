51471855be2223f3bfa947d1a9dd6f5cc32ddefc
shiying <shiying.wei@ucl.ac.uk>
2025-06-18 14:43:27 +0100

    Add assertion tests

--- mod_app/tests/test_views/test_view_context_data.py
+++ mod_app/tests/test_views/test_view_context_data.py
@@ -17,15 +17,19 @@ class TestViewContextData(TestCase):
 
     def test_film_detailview_context(self):
         response = self.client.get(reverse("film_detail", args=[self.film.pk]))
+        self.assertEqual(response.context["film"], self.film)
 
     def test_analysis_detailview_context(self):
         response = self.client.get(reverse("analysis_detail", args=[self.analysis.pk]))
+        self.assertEqual(response.context["analysis"], self.analysis)
 
     def test_tr_detailview_context(self):
         response = self.client.get(reverse("tr_detail", args=[self.tr.pk]))
+        self.assertEqual(response.context["tr"], self.tr)
 
     def test_tag_listview_context(self):
         response = self.client.get(reverse("tag_list"))
 
     def test_tag_detailview_context(self):
         response = self.client.get(reverse("tag_detail", args=[self.tag.pk]))
+        self.assertEqual(response.context["tag"], self.tag)
