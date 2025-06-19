4c43fe48af48eb5ac1f379511781c4e56323abcb
shiying <shiying.wei@ucl.ac.uk>
2025-06-19 13:52:24 +0100

    Add in placeholder assertions for detail views

--- mod_app/tests/test_views/test_view_context_data.py
+++ mod_app/tests/test_views/test_view_context_data.py
@@ -14,14 +14,43 @@ class TestViewContextData(TestCase):
 
     def test_homeview_context(self):
         response = self.client.get(reverse("home"))
+        self.assertQuerysetEqual(
+            response.context["slides"],
+            self.slides_set,
+        )
+        self.assertQuerysetEqual(
+            response.context["slide_images"],
+            False,
+        )
+
+    def test_film_listview_context(self):
+        response = self.client.get(reverse("film_list"))
+        self.assertQuerysetEqual(
+            response.context["film_list"],
+            self.film_set,
+        )
 
     def test_film_detailview_context(self):
         response = self.client.get(reverse("film_detail", args=[self.film.pk]))
         self.assertEqual(response.context["film"], self.film)
 
+    def test_analysis_listview_context(self):
+        response = self.client.get(reverse("analyses_list"))
+        self.assertQuerysetEqual(
+            response.context["analyses_list"],
+            self.analysis_set,
+        )
+
     def test_analysis_detailview_context(self):
         response = self.client.get(reverse("analysis_detail", args=[self.analysis.pk]))
         self.assertEqual(response.context["analysis"], self.analysis)
+
+    def test_tr_listview_context(self):
+        response = self.client.get(reverse("tr_list"))
+        self.assertQuerysetEqual(
+            response.context["tr_list"],
+            self.tr_set,
+        )
 
     def test_tr_detailview_context(self):
         response = self.client.get(reverse("tr_detail", args=[self.tr.pk]))
@@ -29,6 +58,10 @@ class TestViewContextData(TestCase):
 
     def test_tag_listview_context(self):
         response = self.client.get(reverse("tag_list"))
+        self.assertQuerysetEqual(
+            response.context["tag_list"],
+            self.tag_set,
+        )
 
     def test_tag_detailview_context(self):
         response = self.client.get(reverse("tag_detail", args=[self.tag.pk]))
