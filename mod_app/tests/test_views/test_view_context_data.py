e07d611a6c76ab7ce22f3d55a0cf27dc87dbe492
shiying <shiying.wei@ucl.ac.uk>
2025-06-20 12:28:47 +0100

    Add test for distinct context in film detail view

--- mod_app/tests/test_views/test_view_context_data.py
+++ mod_app/tests/test_views/test_view_context_data.py
@@ -27,7 +27,7 @@ class TestViewContextData(TestCase):
     def setUpTestData(cls):
         cls.client = Client()
 
-        film1 = Film.objects.create(title="Test Film1", release_date="2020")
+        cls.film1 = Film.objects.create(title="Test Film1", release_date="2020")
         film2 = Film.objects.create(title="Test Film2", release_date="2020")
         film3 = Film.objects.create(title="Test Film3", release_date="2020")
 
@@ -47,25 +47,25 @@ class TestViewContextData(TestCase):
         bib2 = BibliographyItem.objects.create(short_citation="Test Bibliography2")
         bib3 = BibliographyItem.objects.create(short_citation="Test Bibliography3")
 
-        pm1 = Script.objects.create(url="", film=film1)
-        pm2 = PressBook.objects.create(url="", film=film1)
-        pm3 = Programme.objects.create(url="", film=film1)
-        pm4 = Publicity.objects.create(url="", film=film1)
+        pm1 = Script.objects.create(url="", film=cls.film1)
+        pm2 = PressBook.objects.create(url="", film=cls.film1)
+        pm3 = Programme.objects.create(url="", film=cls.film1)
+        pm4 = Publicity.objects.create(url="", film=cls.film1)
 
-        vr1 = Still.objects.create(url="", film=film1)
-        vr2 = Drawing.objects.create(url="", film=film1)
-        vr3 = Poster.objects.create(url="", film=film1)
-        vr4 = Postcard.objects.create(url="", film=film1)
+        vr1 = Still.objects.create(url="", film=cls.film1)
+        vr2 = Drawing.objects.create(url="", film=cls.film1)
+        vr3 = Poster.objects.create(url="", film=cls.film1)
+        vr4 = Postcard.objects.create(url="", film=cls.film1)
 
-        video1 = Video.objects.create(url="", film=film1)
-        video2 = Video.objects.create(url="", film=film1)
-        video3 = Video.objects.create(url="", film=film1)
+        video1 = Video.objects.create(url="", film=cls.film1)
+        video2 = Video.objects.create(url="", film=cls.film1)
+        video3 = Video.objects.create(url="", film=cls.film1)
 
         cls.test_pm_slides = [pm1, pm2, pm3, pm4]
         cls.test_vr_slides = [vr1, vr2, vr3, vr4]
         cls.test_video_slides = [video1, video2, video3]
 
-        cls.test_films = [film1, film2, film3]
+        cls.test_films = [cls.film1, film2, film3]
         cls.test_analyses = [analysis1, analysis2, analysis3]
         cls.test_trs = [tr1, tr2, tr3]
         cls.test_tags = [tag1, tag2, tag3]
@@ -101,8 +101,17 @@ class TestViewContextData(TestCase):
                     with self.subTest(test_set=test_obj):
                         self.assertIn(test_obj, response.context[context])
 
-    def test_film_detailview_printed_material_slides(): ...
+    def test_film_detailview_printed_material_slides(self):
+        response = self.client.get(reverse("film_detail", args=[self.film1.pk]))
+        self.assertCountEqual(self.test_pm_slides, response.context["pm_slides"])
+        print(response.context["pm_slides"])
 
-    def test_film_detailview_visual_resources_slides(): ...
+    def test_film_detailview_visual_resources_slides(self):
+        response = self.client.get(reverse("film_detail", args=[self.film1.pk]))
+        self.assertCountEqual(self.test_vr_slides, response.context["vr_slides"])
+        print(response.context["vr_slides"])
 
-    def test_film_detailview_video_slides(): ...
+    def test_film_detailview_video_slides(self):
+        response = self.client.get(reverse("film_detail", args=[self.film1.pk]))
+        self.assertCountEqual(self.test_video_slides, response.context["video_slides"])
+        print(response.context["video_slides"])
