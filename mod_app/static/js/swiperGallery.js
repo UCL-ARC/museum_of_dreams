baf844a425734f33ef1960b94cf015d6842956e9
shiying <shiying.wei@ucl.ac.uk>
2025-06-26 11:52:00 +0100

    Intialise basic swiper

--- mod_app/static/js/swiperGallery.js
+++ mod_app/static/js/swiperGallery.js
@@ -1,5 +1,17 @@
-const swiper = new Swiper(".swiper", {
-  autoplay: {
-    delay: 5000,
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
+
+console.log("swiper check");
