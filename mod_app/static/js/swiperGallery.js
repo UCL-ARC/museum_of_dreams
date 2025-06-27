12280a47eab0baaf1bb909adbaf00f392f3d7c32
shiying <shiying.wei@ucl.ac.uk>
2025-06-27 14:19:00 +0100

    Clean up swiper in npm and import esm module correctly

--- mod_app/static/js/swiperGallery.js
+++ mod_app/static/js/swiperGallery.js
@@ -1,8 +1,10 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
 document.addEventListener("DOMContentLoaded", function () {
   const swiper = new Swiper(".swiper", {
     loop: true,
     slidesPerView: 1,
-    spaceBetween: 10,
+    spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
