c5fa84a7a17ce1c8bc5141c0d76cb03b2bd7de5a
shiying <shiying.wei@ucl.ac.uk>
2025-07-02 13:33:16 +0100

    Additional parameters

--- mod_app/static/js/swiperGallery.js
+++ mod_app/static/js/swiperGallery.js
@@ -3,9 +3,12 @@ import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 document.addEventListener("DOMContentLoaded", function () {
   const slides = document.querySelectorAll(".swiper-slide");
   const swiper = new Swiper(".swiper", {
+    grabCursor: true,
+    centeredSlides: true,
     loop: slides.length > 1,
     slidesPerView: 1,
-    spaceBetween: 10,
+    spaceBetween: 0,
+    effect: "coverflow",
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
