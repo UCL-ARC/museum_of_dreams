1be29305a5bf8f15892fa0cd84fa598bf3d8854b
shiying <shiying.wei@ucl.ac.uk>
2025-07-02 13:15:50 +0100

    add loop

--- mod_app/static/js/swiperGallery.js
+++ mod_app/static/js/swiperGallery.js
@@ -1,7 +1,9 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 
 document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".swiper-slide");
   const swiper = new Swiper(".swiper", {
+    loop: slides.length > 1,
     slidesPerView: 1,
     spaceBetween: 10,
     pagination: {
