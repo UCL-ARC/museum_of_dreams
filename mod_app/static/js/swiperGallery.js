67ef52810ba896dbe4f5da3288027bfec9fa9a00
shiying <shiying.wei@ucl.ac.uk>
2025-07-17 15:53:09 +0100

    Disable grab cursor so hovering over description is more intuitive

--- mod_app/static/js/swiperGallery.js
+++ mod_app/static/js/swiperGallery.js
@@ -1,8 +1,8 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
-
+export { swiper };
 const swiperSlides = document.querySelectorAll(".swiper-slide");
 const swiper = new Swiper(".swiper", {
-  grabCursor: true,
+  // grabCursor: true,
   centeredSlides: true,
 
   // loop and rewind conflicts with each other, so it shouldn't be set to true at the same time
@@ -19,27 +19,4 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-});
-
-// when clicking on the active slide, open a window
-console.log(swiperSlides);
-
-let activeSlide = document.getElementsByClassName("swiper-slide-active");
-const modalImage = activeSlide[0].getElementsByTagName("img")[0];
-const closeBtn = document.getElementsByClassName("swiper-modal__btn--close")[0];
-
-console.log("active slides:" + activeSlide);
-console.log(modalImage);
-const modal = document.getElementsByClassName("swiper-modal")[0];
-
-// want to apply to image and video as well later
-modalImage.addEventListener("click", function () {
-  modal.appendChild(modalImage.cloneNode(true));
-  // open modal
-  console.log("test click");
-  modal.showModal();
-});
-
-closeBtn.addEventListener("click", function () {
-  modal.close();
 });
