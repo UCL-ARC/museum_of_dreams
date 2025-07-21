05d772bdeb222f3bd8a8f904f130cf69847962d4
shiying <shiying.wei@ucl.ac.uk>
2025-07-21 12:12:40 +0100

    revert film slide context to the object itself, instead of just the url (to allow displaying description of the object)

--- mod_app/views.py
+++ mod_app/views.py
@@ -79,10 +79,10 @@ class FilmDetailView(DetailView):
             + list(film.postcards.all())
         )
 
-        context["pm_slides"] = [pm_slide.url for pm_slide in printed_material_slides]
-        context["vr_slides"] = [vr_slide.url for vr_slide in visual_resources_slides]
+        context["pm_slides"] = printed_material_slides
+        context["vr_slides"] = visual_resources_slides
 
-        context["video_slides"] = [video_slide.url for video_slide in film.videos.all()]
+        context["video_slides"] = film.videos.all()
         return context
 
 
