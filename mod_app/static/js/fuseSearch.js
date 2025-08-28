8332c65d6c1f7b9c69821c779242a970c692ef90
shiying <shiying.wei@ucl.ac.uk>
2025-08-28 13:04:43 +0100

    fetch form data on sumbit

--- mod_app/static/js/fuseSearch.js
+++ mod_app/static/js/fuseSearch.js
@@ -1,13 +1,22 @@
 import Fuse from "https://cdn.jsdelivr.net/npm/fuse.js@7.1.0/dist/fuse.mjs";
 
-const options = {
-  includeScore: true,
-};
+console.log("search script loaded");
+document.addEventListener("DOMContentLoaded", () => {});
+const filmSearchForm = document.getElementById("film-search-form");
+// const analysisSearchForm = document.getElementById("analysis-search-form");
+filmSearchForm.addEventListener("submit", (event) => {
+  event.preventDefault;
+  let form = getData(event.target);
+  let formData = new FormData(form);
+  // output as an object
+  console.log(Object.fromEntries(formData));
+});
+// analysisSearchForm.addEventListener("submit", (event) => {});
 
-const fuse = new Fuse(list, options);
+// const options = {
+//   includeScore: true,
+// };
 
-const result = fuse.search("od man");
+// const fuse = new Fuse(list, options);
 
-function getSearchForm() {
-  document.getElementById;
-}
+// const result = fuse.search("od man");
