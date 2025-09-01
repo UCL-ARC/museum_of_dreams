7a36f4197c0ba8912ff5a97509f491628616dbeb
shiying <shiying.wei@ucl.ac.uk>
2025-09-01 17:50:50 +0100

    Import context data from json values

--- mod_app/static/js/fuseSearch.js
+++ mod_app/static/js/fuseSearch.js
@@ -1,34 +1,14 @@
 import Fuse from "https://cdn.jsdelivr.net/npm/fuse.js@7.1.0/dist/fuse.mjs";
 
-// if its films or analyses
-// function getContextData(object_type) {
-//   if (object_type === "film") {
-const titles = document.querySelectorAll(".card__body__title");
-const yearOfReleases = document.querySelectorAll(".card__release-date");
-const synopses = document.querySelectorAll(".card__body__description");
+// const titles = document.querySelectorAll(".card__body__title");
+// const yearOfReleases = document.querySelectorAll(".card__release-date");
+// const synopses = document.querySelectorAll(".card__body__description");
+// console.log("Film titles:", titles);
+// console.log("Film years:", yearOfReleases);
+// console.log("Film synopsis:", synopses);
 
-let titleArray = Array.from(titles);
-titleArray.forEach((title) => {
-  title = title.innerHTMl;
-});
-
-// fetch("/film_data")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//   });
-
-console.log("Film titles:", titles);
-console.log("Film years:", yearOfReleases);
-console.log("Film synopsis:", synopses);
-//   }
-//   elif(object_type === "analysis");
-//   {
-//     const title = document.querySelectorAll(".card__body__title");
-//     const yearOfRelease = document.querySelectorAll(".card__release-date");
-//     const synopsis = document.querySelectorAll(".card__body__description");
-//   }
-// }
+const films = JSON.parse(document.getElementById("film_data").innerHTML);
+console.log(films);
 
 // const options = {
 //   includeScore: true,
@@ -46,11 +26,8 @@ const filmSearchForm = document.getElementById("film-search");
 // const analysisSearchForm = document.getElementById("analysis-search-form");
 filmSearchForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  // const form = new FormData(event.target);
-  // // output as an object
-  // console.log(form.entries());
-  const valueById = document.getElementById("search-bar").value;
-  console.log("By ID:", valueById);
+  const searchValue = document.getElementById("search-bar").value;
+  console.log("Search Value:", searchValue);
 });
 
 // if theres 4 digit numbers
