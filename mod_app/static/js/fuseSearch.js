3ae638f36aa9d7993cde5a5ba8f6780b92520fc1
shiying <shiying.wei@ucl.ac.uk>
2025-09-01 13:51:03 +0100

    convert nodelist to innerhtml

--- mod_app/static/js/fuseSearch.js
+++ mod_app/static/js/fuseSearch.js
@@ -3,9 +3,24 @@ import Fuse from "https://cdn.jsdelivr.net/npm/fuse.js@7.1.0/dist/fuse.mjs";
 // if its films or analyses
 // function getContextData(object_type) {
 //   if (object_type === "film") {
-//     const title = document.querySelectorAll(".card__body__title");
-//     const yearOfRelease = document.querySelectorAll(".card__release-date");
-//     const synopsis = document.querySelectorAll(".card__body__description");
+const titles = document.querySelectorAll(".card__body__title");
+const yearOfReleases = document.querySelectorAll(".card__release-date");
+const synopses = document.querySelectorAll(".card__body__description");
+
+let titleArray = Array.from(titles);
+titleArray.forEach((title) => {
+  title = title.innerHTMl;
+});
+
+// fetch("/film_data")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+console.log("Film titles:", titles);
+console.log("Film years:", yearOfReleases);
+console.log("Film synopsis:", synopses);
 //   }
 //   elif(object_type === "analysis");
 //   {
@@ -27,7 +42,6 @@ import Fuse from "https://cdn.jsdelivr.net/npm/fuse.js@7.1.0/dist/fuse.mjs";
 
 console.log("search script loaded");
 const filmSearchForm = document.getElementById("film-search");
-console.log(filmSearchForm);
 
 // const analysisSearchForm = document.getElementById("analysis-search-form");
 filmSearchForm.addEventListener("submit", function (event) {
@@ -38,6 +52,11 @@ filmSearchForm.addEventListener("submit", function (event) {
   const valueById = document.getElementById("search-bar").value;
   console.log("By ID:", valueById);
 });
+
+// if theres 4 digit numbers
+// film title
+// film synopsis
+
 // analysisSearchForm.addEventListener("submit", (event) => {});
 
 // const options = {
