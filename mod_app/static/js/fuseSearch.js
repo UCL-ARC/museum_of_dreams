7b9724b1df53bd5140365043385e127993ee69fb
shiying <shiying.wei@ucl.ac.uk>
2025-09-01 13:24:34 +0100

    Get form data via id

--- mod_app/static/js/fuseSearch.js
+++ mod_app/static/js/fuseSearch.js
@@ -1,15 +1,42 @@
 import Fuse from "https://cdn.jsdelivr.net/npm/fuse.js@7.1.0/dist/fuse.mjs";
 
+// if its films or analyses
+// function getContextData(object_type) {
+//   if (object_type === "film") {
+//     const title = document.querySelectorAll(".card__body__title");
+//     const yearOfRelease = document.querySelectorAll(".card__release-date");
+//     const synopsis = document.querySelectorAll(".card__body__description");
+//   }
+//   elif(object_type === "analysis");
+//   {
+//     const title = document.querySelectorAll(".card__body__title");
+//     const yearOfRelease = document.querySelectorAll(".card__release-date");
+//     const synopsis = document.querySelectorAll(".card__body__description");
+//   }
+// }
+
+// const options = {
+//   includeScore: true,
+// };
+
+// function search(list, query) {
+//   const fuse = new Fuse(list, options);
+//   const result = fuse.search(query);
+//   return result;
+// }
+
 console.log("search script loaded");
-document.addEventListener("DOMContentLoaded", () => {});
-const filmSearchForm = document.getElementById("film-search-form");
+const filmSearchForm = document.getElementById("film-search");
+console.log(filmSearchForm);
+
 // const analysisSearchForm = document.getElementById("analysis-search-form");
-filmSearchForm.addEventListener("submit", (event) => {
-  event.preventDefault;
-  let form = getData(event.target);
-  let formData = new FormData(form);
-  // output as an object
-  console.log(Object.fromEntries(formData));
+filmSearchForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // const form = new FormData(event.target);
+  // // output as an object
+  // console.log(form.entries());
+  const valueById = document.getElementById("search-bar").value;
+  console.log("By ID:", valueById);
 });
 // analysisSearchForm.addEventListener("submit", (event) => {});
 
