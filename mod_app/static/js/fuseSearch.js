28fe0b30a27ee2ca2f7d1acca0701cab358f67fe
shiying <shiying.wei@ucl.ac.uk>
2025-09-02 15:31:04 +0100

    Request a json response and apply fuse search

--- mod_app/static/js/fuseSearch.js
+++ mod_app/static/js/fuseSearch.js
@@ -6,40 +6,86 @@ import Fuse from "https://cdn.jsdelivr.net/npm/fuse.js@7.1.0/dist/fuse.mjs";
 // console.log("Film titles:", titles);
 // console.log("Film years:", yearOfReleases);
 // console.log("Film synopsis:", synopses);
+// const films = JSON.parse(document.getElementById("film_data"));
+// console.log(films);
+// console.log(typeof films);
 
-const films = JSON.parse(document.getElementById("film_data").innerHTML);
-console.log(films);
+// const films_dict = films.map(({ key, value }) => ({ [key]: value }));
+// console.log(films_dict);
+// console.log(Array.isArray(films));
 
-// const options = {
-//   includeScore: true,
-// };
+// const film_list = {};
 
-// function search(list, query) {
-//   const fuse = new Fuse(list, options);
-//   const result = fuse.search(query);
-//   return result;
-// }
+// films.forEach((film) => {
+//   console.log(film.pk, film.title);
+// });
 
 console.log("search script loaded");
-const filmSearchForm = document.getElementById("film-search");
 
-// const analysisSearchForm = document.getElementById("analysis-search-form");
-filmSearchForm.addEventListener("submit", function (event) {
+// get search value
+const fuseOptions = {
+  // Search in `author` and in `tags` array
+  threshold: 0.3,
+  keys: ["title", "release_date", "synopsis"],
+};
+
+function search(list, query, options) {
+  const fuse = new Fuse(list, options);
+  const result = fuse.search(query);
+  console.log("Search successful:", result);
+}
+
+const filmSearchForm = document.getElementById("film-search");
+console.log(window.location.origin);
+
+filmSearchForm.addEventListener("submit", async (event) => {
+  const url = window.location.origin + "/films?film_data=json";
   event.preventDefault();
-  const searchValue = document.getElementById("search-bar").value;
-  console.log("Search Value:", searchValue);
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    const filmData = await response.json();
+    // const filmDataArray = Object.values(filmData);
+    console.log("Data received:", filmData);
+    console.log(typeof filmData);
+    console.log(Array.isArray(filmData));
+    console.log("First film:", filmData[1]);
+    const searchValue = document.getElementById("search-bar").value;
+    console.log("Search Value:", searchValue);
+
+    search(filmData, searchValue, fuseOptions);
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
 });
+
+//fire request to get film data
+
+// gets a json response
+
+// fuse sort through the response and display the films
 
 // if theres 4 digit numbers
 // film title
 // film synopsis
 
 // analysisSearchForm.addEventListener("submit", (event) => {});
-
-// const options = {
-//   includeScore: true,
-// };
-
-// const fuse = new Fuse(list, options);
-
-// const result = fuse.search("od man");
+const fuseTestData = [
+  {
+    title: "Old Man's War",
+    author: "John Scalzi",
+    tags: ["fiction"],
+  },
+  {
+    title: "The Lock Artist",
+    author: "Steve",
+    tags: ["thriller"],
+  },
+];
+console.log(fuseTestData);
+// const fuse = new Fuse(fuseTestData, fuseOptions);
+// const result = fuse.search("old");
+// console.log(result);
+search(fuseTestData, "old", fuseOptions);
