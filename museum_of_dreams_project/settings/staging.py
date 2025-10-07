d850d6c694bf46f8e9912eb679cdba828320e46d
shiying <shiying.wei@ucl.ac.uk>
2025-10-07 12:28:55 +0100

    Fetch elasticbeanstalk env name from env variable for custom allowed ip

--- museum_of_dreams_project/settings/staging.py
+++ museum_of_dreams_project/settings/staging.py
@@ -1,14 +1,25 @@
-from .aws import *
-from infrastructure.stacks.staging_stack import STAGING_ENV_NAME
+import os
 import re
 
+from .aws import *
+
+AWS_STAGING_ENV_NAME = os.environ.get("AWS_STAGING_ENV_NAME", "none")
+
 ENVIRONMENT = "staging"
-ALLOWED_HOSTS = [
-    re.compile(rf"^{STAGING_ENV_NAME}\.[a-zA-Z0-9]\.eu-west-2\.elasticbeanstalk\.com$"),
-    "museumofdreams.eu-west-2.elasticbeanstalk.com",
-    "staging.museumofdreamworlds.org",
-    LOCAL_IP,
-]
+if AWS_STAGING_ENV_NAME:
+    ALLOWED_HOSTS = [
+        re.compile(
+            rf"^{AWS_STAGING_ENV_NAME}\.[a-zA-Z0-9]\.eu-west-2\.elasticbeanstalk\.com$"
+        ),
+        LOCAL_IP,
+    ]
+else:
+    ALLOWED_HOSTS = [
+        "museumofdreams.eu-west-2.elasticbeanstalk.com",
+        "staging.museumofdreamworlds.org",
+        LOCAL_IP,
+    ]
+
 GRAPPELLI_ADMIN_TITLE = "Museum of Dreams"
 AWS_S3_CUSTOM_DOMAIN = (
     f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
